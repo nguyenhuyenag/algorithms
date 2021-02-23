c4a5b3b07921ea77e7ff05e3178e53248944d25c
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2021-02-23 17:02:31 +0700

    #

--- codewars/src/main/java/codewars/number/SortTheOdd.java
+++ codewars/src/main/java/codewars/number/SortTheOdd.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 // Sắp xếp số lẻ tăng dần, số chẵn giữ nguyên vị trí
 public class SortTheOdd {
 
-	public static int[] sortArray(int[] arr) {
+	public static int[] bubbleSort(int[] arr) {
 		int n = arr.length;
 		for (int i = 0; i < n - 1; i++) {
 			for (int j = i + 1; j < n; j++) {
@@ -24,7 +24,7 @@ public class SortTheOdd {
 	public static void main(String[] args) {
 		int[] arr = { 23, 1, 9, 8, 5, 0, 7, 12 };
 		// bubbleSort(arr);
-		System.out.println(Arrays.toString(sortArray(arr)));
+		System.out.println(Arrays.toString(bubbleSort(arr)));
 	}
 
 }
