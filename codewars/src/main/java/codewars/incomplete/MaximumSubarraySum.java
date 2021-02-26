ffb51d8cbe9284ff6c5c0bb0417837d3933d579e
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2021-02-26 17:03:33 +0700

    #

---
+++ codewars/src/main/java/codewars/incomplete/MaximumSubarraySum.java
@@ -0,0 +1,27 @@
+package codewars.incomplete;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+// https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c
+public class MaximumSubarraySum {
+
+	public static int sequence(int[] arr) {
+		int max = 0, csum = 0;
+		for (int x : arr) {
+			csum = Math.max(0, csum + x);
+			max = Math.max(max, csum);
+		}
+		return max;
+	}
+
+	@Test
+	public void testEmptyArray() throws Exception {
+		// assertEquals("Empty arrays should have a max of 0", 0, sequence(new int[]
+		// {}));
+		assertEquals(7, sequence(new int[] { -2, -3, 4, -1, -2, 1, 5, -3 }));
+		assertEquals(6, sequence(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+	}
+
+}
