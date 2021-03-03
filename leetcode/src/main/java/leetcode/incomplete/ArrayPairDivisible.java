bb55cf7faf1d58e18a0720d5102cf9fd944d5f54
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2021-03-03 17:18:47 +0700

    #

--- leetcode/src/main/java/leetcode/incomplete/ArrayPairDivisible.java
+++ leetcode/src/main/java/leetcode/incomplete/ArrayPairDivisible.java
@@ -13,45 +13,51 @@ import org.junit.Test;
  */
 public class ArrayPairDivisible {
 
+	public int[] findPair(List<Integer> list, int k) {
+		int n = list.size();
+		int stop = n / 2;
+		for (int i = 0; i < n; i++) {
+			int a = list.get(i);
+			if (i >= stop) {
+				return new int[] { -1, -1 };
+			}
+			for (int j = i + 1; j < n; j++) {
+				int b = list.get(j);
+				if ((a + b) % k == 0) {
+					return new int[] { i, j };
+				}
+			}
+		}
+		return new int[] { -1, -1 };
+	}
+
 	public boolean canArrange(int[] A, int k) {
 		int count = 0;
 		int stop = A.length / 2;
 		List<Integer> list = Arrays.stream(A).boxed().collect(Collectors.toList());
-		// [1, 2, 3, 4, 5, 10, 6, 7, 8, 9]
 		while (list.size() > 0) {
-			int[] rm = new int[2];
-			for (int i = 0; i < list.size(); i++) {
-				int a = list.get(i);
-				for (int j = i + 1; j < list.size(); j++) {
-					int b = list.get(j);
-					if ((a + b) % k == 0) { // a + b chia het cho k
-						count++;
-						rm[1] = j;
-						break;
-					}
-					if (count == stop) {
-						return true;
-					}
+			int[] pair = findPair(list, k);
+			if (pair[0] != -1) {
+				count++;
+				if (count == stop) {
+					return true;
 				}
-				if (rm[0] != rm[1]) {
-					break;
-				}
+				list.remove(pair[1]);
+				list.remove(pair[0]);
+			} else {
+				return false;
 			}
-			if (rm[0] != rm[1]) {
-				list.remove(rm[1]);
-			}
-			list.remove(0);				
 		}
 		return false;
 	}
 
 	@Test
 	public void test() {
-		// assertEquals(true, canArrange(new int[] { -10, 10 }, 2));
+		assertEquals(true, canArrange(new int[] { -10, 10 }, 2));
 		assertEquals(true, canArrange(new int[] { 1, 2, 3, 4, 5, 10, 6, 7, 8, 9 }, 5));
-		// assertEquals(true, canArrange(new int[] { 1, 2, 3, 4, 5, 6 }, 7));
-		// assertEquals(false, canArrange(new int[] { 1, 2, 3, 4, 5, 6 }, 10));
-		// assertEquals(true, canArrange(new int[] { -1,1,-2,2,-3,3,-4,4 }, 3));
+		assertEquals(true, canArrange(new int[] { 1, 2, 3, 4, 5, 6 }, 7));
+		assertEquals(false, canArrange(new int[] { 1, 2, 3, 4, 5, 6 }, 10));
+		assertEquals(true, canArrange(new int[] { -1, 1, -2, 2, -3, 3, -4, 4 }, 3));
 	}
 
 }
