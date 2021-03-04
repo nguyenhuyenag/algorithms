a6cf1473cf3a3c1862bcc22243bb32cbbdcb5b26
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2021-03-04 15:34:28 +0700

    #

---
+++ leetcode/src/main/java/leetcode/number/UglyNumber.java
@@ -0,0 +1,37 @@
+package leetcode.number;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/*-
+ * Write a program to check whether a given number is an ugly number.
+ * 
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5
+ * 
+ * https://leetcode.com/problems/ugly-number/
+ */
+public class UglyNumber {
+
+	public boolean isUgly(int n) {
+		if (n == 0) {
+			return false;
+		}
+		int[] divisors = { 2, 3, 5 };
+		for (int d : divisors) {
+			while (n % d == 0) {
+				n /= d;
+			}
+		}
+		return n == 1;
+	}
+
+	@Test
+	public void test() {
+		assertEquals(true, isUgly(1));
+		assertEquals(true, isUgly(6));
+		assertEquals(true, isUgly(8));
+		assertEquals(false, isUgly(14));
+	}
+
+}
