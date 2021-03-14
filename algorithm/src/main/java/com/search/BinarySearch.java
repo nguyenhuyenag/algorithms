9512a68b33b2c7f8643f8444b61302b60527592c
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2021-03-14 14:07:53 +0700

    #

---
+++ algorithm/src/main/java/com/search/BinarySearch.java
@@ -0,0 +1,31 @@
+package com.search;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+	public static int binarySearch(int[] A, int key) {
+		int l = 0;
+		int r = A.length - 1;
+		while (l <= r) {
+			int m = (l + r) / 2;
+			if (A[m] == key) {
+				return m;
+			}
+			if (A[m] < key) {
+				l = m + 1;
+			} else {
+				r = m - 1;
+			}
+		}
+		return -1; // key not found
+	}
+
+	public static void main(String[] args) {
+		int[] A = { 1, -9, 6, 11, 21, 6, 7, 90, 9, 1 };
+		Arrays.sort(A);
+		System.out.println(Arrays.toString(A));
+		System.out.println(binarySearch(A, 11));
+	}
+
+}
