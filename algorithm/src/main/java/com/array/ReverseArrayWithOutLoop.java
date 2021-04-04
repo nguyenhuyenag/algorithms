38959d69eff1b672ef214622883d3be3a3759252
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2021-04-04 21:33:38 +0700

    #

---
+++ algorithm/src/main/java/com/array/ReverseArrayWithOutLoop.java
@@ -0,0 +1,18 @@
+package com.array;
+
+public class ReverseArrayWithOutLoop {
+
+	public static void show(int[] A, int n) {
+		if (n >= 0) {
+			System.out.println(A[n]);
+			show(A, n - 1);
+		}
+		return;
+	}
+
+	public static void main(String[] args) {
+		int[] A = { 1, 2, 3, 4, 5 };
+		show(A, A.length - 1);
+	}
+
+}
