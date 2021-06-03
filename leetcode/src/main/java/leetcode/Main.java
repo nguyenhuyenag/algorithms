2c60f4bff0893e9410d882a5bb695bbb2160e5f5
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2021-06-03 17:18:02 +0700

    update

--- leetcode/src/main/java/leetcode/Main.java
+++ leetcode/src/main/java/leetcode/Main.java
@@ -1,6 +1,20 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class Main {
+	
+	public int[] plusOne(int[] digits) {
+		int ONE = 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+        	digits[i] += ONE;
+        	if (digits[i] <= 9) {
+        		return digits;
+        	}
+        	digits[i] = 1;
+        }
+        return digits;
+    }
 
 	public static void main(String[] args) {
 		
---
+++ leetcode/src/main/java/leetcode/array/MoveZeroes.java
@@ -0,0 +1,45 @@
+package leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/move-zeroes/
+ */
+public class MoveZeroes {
+
+	public static void move(int[] A, int k) {
+		int i;
+		int tmp = A[k];
+		for (i = k; i < A.length - 1; i++) {
+			A[i] = A[i + 1];
+		}
+		A[i] = tmp;
+	}
+
+	public static void moveZeroes(int[] nums) {
+		int n = nums.length;
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			if (nums[i] == 0 && i != n - 1) {
+				count++;
+			}
+		}
+		for (int i = 1; i <= count; i++) {
+			for (int j = 0; j < n; j++) {
+				if (nums[j] == 0) {
+					move(nums, j);
+					break;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 0, 1, 0, 3, 12 };
+		// int[] nums = { 0 };
+		// int[] nums = { 0, 0, 1 };
+		moveZeroes(nums);
+		System.out.println(Arrays.toString(nums));
+	}
+
+}
