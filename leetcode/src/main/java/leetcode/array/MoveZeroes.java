640ca653267176b9fff31ac96e1ef49c34e8ab9b
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-01-20 11:39:16 +0700

    #

--- leetcode/src/main/java/leetcode/array/MoveZeroes.java
+++ leetcode/src/main/java/leetcode/array/MoveZeroes.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 
 /**
  * https://leetcode.com/problems/move-zeroes/
+ * 
+ * Given an integer array nums, move all 0's to the end of it while maintaining
+ * the relative order of the non-zero elements.
+ * 
+ * Note that you must do this in-place without making a copy of the array.
  */
 public class MoveZeroes {
 
