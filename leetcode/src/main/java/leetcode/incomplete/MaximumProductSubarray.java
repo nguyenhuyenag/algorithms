6f2ef956333ca6b6974d84d68ded006576e6c9b8
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-08-24 17:14:57 +0700

    #

---
+++ leetcode/src/main/java/leetcode/incomplete/MaximumProductSubarray.java
@@ -0,0 +1,23 @@
+package leetcode.incomplete;
+
+public class MaximumProductSubarray {
+
+	public int maxProduct(int[] A) {
+		int n = A.length;
+		int max = 1, cmax = 1;
+		for (int i = 0; i < n; i++) {
+			max = A[i];
+			for (int j = i + 1; j < n; i++) {
+				if () {
+					
+				}
+			}
+		}
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 2, 3, -2, 4 };
+	}
+
+}
--- leetcode/src/main/java/leetcode/number/PowerofFour.java
+++ leetcode/src/main/java/leetcode/number/PowerofFour.java
@@ -16,6 +16,16 @@ public class PowerofFour {
 		}
 		return true;
 	}
+	
+	public boolean isPowerOfThree2(int n) {
+        if (n < 1) {
+            return false;
+        }
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
 
 	public static boolean isPowerOfAny(int n, int k) {
 		if (n <= 0) {
@@ -32,7 +42,12 @@ public class PowerofFour {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(isPowerOfFour(8));
+		// System.out.println(isPowerOfFour(8));
+		int i = 0;
+		while (i < Integer.MAX_VALUE && Math.pow(3, i) < Integer.MAX_VALUE) {
+			System.out.println((int) Math.pow(3, i));
+			i++;
+		}
 	}
 
 }
