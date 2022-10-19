7e52405b6135c0a73ea14dc782ae1b4e468efe60
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-10-19 10:34:06 +0700

    #

--- leetcode/src/main/java/leetcode/Main.java
+++ leetcode/src/main/java/leetcode/Main.java
@@ -34,12 +34,40 @@ public class Main {
 		String str = "leetcode";
 		BitSet[] alphabet = new BitSet[26];
 		for (char c : str.toCharArray()) {
-			System.out.println(c - 'a');
+			// System.out.println(c - 'a');
 			// alphabet.set(alphabet[c - 'a']);
-			// alphabet[c - 'a'].get(bitIndex)
+			// alphabet[c - 'a']
 		}
-		// BitSet bs = new BitSet(1);
-		// System.out.println(bs.get(0));
+		//
+		BitSet bits1 = new BitSet(16);
+		BitSet bits2 = new BitSet(16);
+
+		// thiet la mot so bit
+		for (int i = 0; i < 16; i++) {
+			if (i % 2 == 0)
+				bits1.set(i);
+			if (i % 5 != 0)
+				bits2.set(i);
+		}
+		// System.out.println("Pattern ban dau trong bits1: ");
+		System.out.println("bits1: " + bits1);
+		// System.out.println("\nPattern ban dau trong bits2: ");
+		System.out.println("bits2: " + bits2);
+
+		// AND bits
+		bits2.and(bits1);
+		System.out.println("\nbits2 AND bits1: ");
+		System.out.println(bits2);
+
+		// OR bits
+		bits2.or(bits1);
+		System.out.println("\nbits2 OR bits1: ");
+		System.out.println(bits2);
+
+		// XOR bits
+		bits2.xor(bits1);
+		System.out.println("\nbits2 XOR bits1: ");
+		System.out.println(bits2);
 	}
 
 }
