506e117d0954015029182079cc4ebb8b68101f22
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-03-16 10:21:27 +0700

    #

--- algorithm/src/main/java/com/linked/LinkedList.java
+++ algorithm/src/main/java/com/linked/LinkedList.java
@@ -2,66 +2,98 @@ package com.linked;
 
 public class LinkedList {
 
-	Node head; // head of the list
+	Node head; // head of list
+	Node tail; // tail of list
 
-	private static class Node {
+	private class Node {
 		int data;
 		Node next;
 
-		// Next is by default initialized as null
-		public Node(int d) {
-			this.data = d;
-			next = null;
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
 		}
 	}
 
 	public void add(int data) {
-		Node new_node = new Node(data);
-		new_node.next = head;
-		head = new_node;
+		Node newNode = new Node(data);
+		if (head == null) {
+			// If list is empty, both head and tail will point to new node
+			head = newNode;
+			tail = newNode;
+		} else {
+			// newNode will be added after tail such that tail's next will point to newNode
+			tail.next = newNode;
+			// newNode will become new tail of the list
+			tail = newNode;
+		}
 	}
 
-	/* This function prints contents of linked list starting from head */
-	public void printList() {
-		Node temp = head;
-		while (temp != null) {
-			System.out.print(temp.data + " -> ");
-			temp = temp.next;
+	public void addFirst(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			// newNode will be added before head such that newNode next will point to head
+			newNode.next = head;
+			// newNode will become new head of the list
+			head = newNode;
 		}
-		System.out.print("Null" + "\n");
 	}
 
-	public int length() {
-		int length = 0;
+//	public void addFirst(int data) {
+//		Node new_node = new Node(data);
+//		new_node.next = head;
+//		head = new_node;
+//	}
+
+	public void showList() {
 		Node temp = head;
 		while (temp != null) {
-			length++;
+			System.out.print(temp.data + " ");
 			temp = temp.next;
 		}
-		return length;
+		// System.out.print("Null" + "\n");
 	}
 
-	public boolean contains(int data) {
-		Node temp = head;
-		while (temp != null) {
-			if (temp.data == data) {
-				return true;
-			}
-			temp = temp.next;
-		}
-		return false;
-	}
+//	public int length() {
+//		int length = 0;
+//		Node temp = head;
+//		while (temp != null) {
+//			length++;
+//			temp = temp.next;
+//		}
+//		return length;
+//	}
+
+//	public boolean contains(int data) {
+//		Node temp = head;
+//		while (temp != null) {
+//			if (temp.data == data) {
+//				return true;
+//			}
+//			temp = temp.next;
+//		}
+//		return false;
+//	}
 
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList(); // Create an empty list
 
-		list.add(1);
-		list.add(2);
-		list.add(3);
+		list.addFirst(1);
+		list.addFirst(2);
+		list.addFirst(3);
+		list.addFirst(4);
 
-		list.printList();
-		System.out.println("Size: " + list.length());
-		System.out.println("Contain: " + list.contains(5));
+//		list.add(1);
+//		list.add(2);
+//		list.add(3);
+
+		// System.out.println("Size: " + list.length());
+		list.showList();
+
+		// System.out.println("Contain: " + list.contains(5));
 
 		/*- Three nodes have been allocated dynamically. 
 		We have references to these three blocks as head,   
