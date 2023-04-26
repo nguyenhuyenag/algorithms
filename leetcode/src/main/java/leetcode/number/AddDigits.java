3de3909c6dfc995a629385987c45dfa74e7c3833
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-04-26 17:17:24 +0700

    #

--- leetcode/src/main/java/leetcode/number/AddDigits.java
+++ leetcode/src/main/java/leetcode/number/AddDigits.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * https://leetcode.com/problems/add-digits/
+ */
 public class AddDigits {
 
 	public static int sumOfDigits(int n) {
@@ -15,15 +18,15 @@ public class AddDigits {
 		return sum;
 	}
 
-	public int addDigits1(int n) {
-		while (true) {
-			n = sumOfDigits(n);
-			if (n < 10) {
-				break;
-			}
-		}
-		return n;
-	}
+//	public int addDigits1(int n) {
+//		while (true) {
+//			n = sumOfDigits(n);
+//			if (n < 10) {
+//				break;
+//			}
+//		}
+//		return n;
+//	}
 
 	public int addDigits2(int n) {
 		while (n >= 10) {
@@ -44,10 +47,25 @@ public class AddDigits {
 		return n;
 	}
 
+	/**
+	 * ABCD = 1000A+100B+10*C+D = (A + B + C + D) + 9 * (111 * A + 11 * B + C) this
+	 * equals (mod 9) to A + B + C + D
+	 */
+	public int addDigits4(int n) {
+		if (n == 0) {
+			return 0;
+		}
+		if (n % 9 == 0) {
+			return 9;
+		}
+		return n % 9;
+	}
+
 	@Test
 	public void test1() {
-		// assertEquals(2, addDigits(38));
-		assertEquals(0, addDigits3(0));
+		int n = 125564;
+		assertEquals((addDigits2(n)), addDigits4(38));
+		// assertEquals(0, addDigits3(0));
 	}
 
 }
