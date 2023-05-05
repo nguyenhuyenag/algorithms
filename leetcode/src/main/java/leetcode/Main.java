f537a78f8e96e3c1987eb12e1115d54136d5b35e
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-05-05 17:14:23 +0700

    #

--- leetcode/src/main/java/leetcode/Main.java
+++ leetcode/src/main/java/leetcode/Main.java
@@ -1,20 +1,44 @@
 package leetcode;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
+// https://voz.vn/t/leetcode-moi-ngay.568742/page-183#post-25048593
+// https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
 public class Main {
+
+	public int maxVowels(String s, int k) {
+		Map<Character, Integer> map = new HashMap<>();
+		for (char c : "aeoui".toCharArray()) {
+			map.put(c, 0);
+		}
+		for (int i = 0; i < s.length() - 3; i++) {
+			String sub = s.substring(i, i + 3);
+			System.out.println(sub);
+		}
+		return 0;
+	}
 
 	@Test
 	public void test() {
-
+		// s = "abciiidef", k = 3
+		assertEquals(3, maxVowels("abciiidef", 3));
 	}
 
 	public static void main(String[] args) {
-		Map<Integer, Integer> map = new HashMap<>();
-		System.out.println(map.getOrDefault(1, 0));
+		String s = "abciiidef";
+		for (int i = 0; i <= s.length() - 3; i++) {
+			System.out.println(s.substring(i, i + 3));
+		}
+		Map<Character, Integer> map = new HashMap<>();
+		for (char c : "aeoui".toCharArray()) {
+			map.put(c, 0);
+		}
+		System.out.println(map);
 	}
 
 }
