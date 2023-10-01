0912f6cdaa5a5caaf11cdf0c50b57f7f40234438
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-10-01 13:33:48 +0700

    #

---
+++ algorithm/src/main/java/com/string/ReverseString.java
@@ -0,0 +1,32 @@
+package com.string;
+
+public class ReverseString {
+
+	public static String reverseString1(String str) {
+		if (str.isEmpty()) {
+			return str;
+		}
+		return reverseString1(str.substring(1)) + str.charAt(0);
+	}
+
+	public static String reverseString2(String original) {
+		char[] chars = original.toCharArray();
+		int left = 0;
+		int right = chars.length - 1;
+		while (left < right) {
+			char temp = chars[left];
+			chars[left] = chars[right];
+			chars[right] = temp;
+			left++;
+			right--;
+		}
+		return new String(chars);
+	}
+
+	public static void main(String[] args) {
+		String str = "Java";
+		System.out.println(reverseString1(str));
+		System.out.println(reverseString2(str));
+		System.out.println(new StringBuilder(str).reverse());
+	}
+}
