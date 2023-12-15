370838df626b83e18fcf0f81406a180ea220151b
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-12-15 09:25:43 +0700

    #

--- leetcode/src/main/java/leetcode/Main.java
+++ leetcode/src/main/java/leetcode/Main.java
@@ -1,40 +1,47 @@
 package leetcode;
 
-/**
- * Đếm số lượng mảng con có k số lẻ
- * <p>
- * https://leetcode.com/problems/count-number-of-nice-subarrays/description/
+/*-
+ * https://leetcode.com/problems/decoded-string-at-index/description/
+ *
+ * Cho chuỗi s và số nguyên k, thực hiện mã hóa chuỗi theo tiêu chí
+ *    
+ *    + Nếu ký tự nhập vào là chữ thì giữ nguyên
+ *    
+ *    + Nếu ký tự nhập vào là số d, lặp lại chuỗi mã hóa hiện tại d - 1 lần
+ * 
+ * Trả về ký tự vị trí thứ k (tính từ 1) của chuỗi mã hóa
  */
 public class Main {
 
-    public static int numberOfSubarrays(int[] nums, int k) {
-        int dem = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            int countLe = 0;
-            for (int j = i; j < n; j++) {
-                if (nums[j] % 2 != 0) {
-                    countLe++;
-                }
-                if (countLe == k) {
-                    dem++;
-                }
-            }
-        }
-        // System.out.println(dem);
-        return dem;
-    }
+	public StringBuilder repeatedString(StringBuilder originalString, int repeatCount) {
+		StringBuilder repeatedString = new StringBuilder();
+		for (int i = 0; i < repeatCount; i++) {
+			repeatedString.append(originalString);
+		}
+		return repeatedString;
+	}
 
-    public static void main(String[] args) {
-        Main m = new Main();
-        // m.numberOfSubarrays(new int[]{2, 4, 6}, 1); // 0
-        // m.numberOfSubarrays(new int[]{1, 1, 2, 1, 1}, 3);
-        m.numberOfSubarrays(new int[]{2, 2, 2, 1, 2, 2, 1, 2, 2, 2}, 2); // 16
-    }
+	public String decodeAtIndex(String s, int k) {
+		StringBuilder ans = new StringBuilder();
+		for (char c : s.toCharArray()) {
+			if (Character.isLetter(c)) {
+				ans.append(c);
+			}
+			if (Character.isDigit(c)) {
+				ans = repeatedString(ans, Character.getNumericValue(c));
+			}
+		}
+		// System.out.println(Character.toString(ans.charAt(k - 1)));
+		return Character.toString(ans.charAt(k + 1));
+	}
 
-//	@org.junit.jupiter.api.Test
-//	public void test() {
-//		// assertEquals("", largestGoodInteger("101010"));
-//	}
+	public static void main(String[] args) {
+		Main m = new Main();
+		m.decodeAtIndex("ha22", 5); // h
+		// m.decodeAtIndex("leet2code3", 10); // o
+		// m.decodeAtIndex("a2345678999999999999999", 1); // a
+		// char t = '9';
+		// System.out.println(t - '2');
+	}
 
 }
--- leetcode/src/main/java/leetcode/number/IntegerBreak.java
+++ leetcode/src/main/java/leetcode/number/IntegerBreak.java
@@ -1,7 +1,5 @@
 package leetcode.number;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
