c88cddbf7f668456d8cbab91c7365fedf54e8965
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-12-24 21:18:00 +0700

    #

--- algorithm/src/main/java/com/Main.java
+++ algorithm/src/main/java/com/Main.java
@@ -1,69 +1,15 @@
 package com;
 
-import java.util.Arrays;
-
-import com.util.RandomUtils;
+import java.util.Stack;
 
 public class Main {
-	
-	// Dịch sang trái 2 bước các phần tử khác A
-	public static void move() {
-		// số bước dịch chuyển
-		final int STEP_MOVE = 2;
-		// chuỗi thực tế cần dịch chuyển
-		StringBuilder sb = new StringBuilder();
-		char arr[] = { 'A', 'B', 'C', 'D', 'A', 'A', 'E' };
-		int n = arr.length;
-		for (int i = 0; i < n; i++) {
-			if (arr[i] != 'A') {
-				sb.append(arr[i]);
-			}
-		}
-		int len = sb.length() - 1;
-		// ở mỗi bước ta dời phần tử cuối lên đầu
-		for (int i = 0; i < STEP_MOVE; i++) {
-			char last = sb.charAt(len);
-			sb.deleteCharAt(len);
-			sb.insert(0, last);
-		}
-		// chèn lại các vị trí của A
-		for (int i = 0; i < n; i++) {
-			if (arr[i] == 'A') {
-				sb.insert(i, "A");
-			}
-		}
-		System.out.println(Arrays.toString(sb.toString().split("")));
-	}
 
-	public static int binarySearch(int[] A, int key) {
-		int l = 0;
-		int r = A.length - 1;
-		while (r - l >= 0) {
-			int m1 = l + (r - l) / 3;
-			int m2 = r - (r - l) / 3;
-			if (A[m1] == key) {
-				return m1;
-			}
-			if (A[m2] == key) {
-				return m2;
-			}
-			if (A[m1] > key) {
-				r = m1 - 1;
-			} else if (A[m2] < key) {
-				l = m2 + 1;
-			} else {
-				l = m1 + 1;
-				r = m2 - 1;
-			}
-		} // while ends
-		return -1;
-	}
-
-	public static void main(String[] args) {
-		int[] A = RandomUtils.randomArrays(10);
-		Arrays.sort(A);
-		System.out.println(Arrays.toString(A));
-		System.out.println(binarySearch(A, A[4]));
-	}
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.add("A");
+        stack.add("B");
+        stack.add("C");
+        System.out.println(String.join("", stack));
+    }
 
 }
--- algorithm/src/main/java/com/recursion/TowerHaNoi.java
+++ algorithm/src/main/java/com/recursion/TowerHaNoi.java
@@ -2,31 +2,18 @@ package com.recursion;
 
 public class TowerHaNoi {
 
-	static int total = 0;
+    public static void solveHanoi(int n, char source, char destination, char auxiliary) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + source + " to " + destination);
+            return;
+        }
+        solveHanoi(n - 1, source, auxiliary, destination);
+        System.out.println("Move disk " + n + " from " + source + " to " + destination);
+        solveHanoi(n - 1, auxiliary, destination, source);
+    }
 
-	static void move(int n, String a, String b, String c) {
-		if (n == 1) {
-			System.out.println(a + " -> " + c);
-			total++;
-			return;
-		}
-		// chuyển n - 1 đĩa từ cọc nguồn sang cọc trung gian,
-		// lấy cọc đích làm cọc phụ
-		move(n - 1, a, c, b);
-		// chuyển đĩa còn lại từ cọc A -> C
-		move(1, a, b, c);
-		// chuyễn n - 1 từ cọc trung gian về cọc đích,
-		// lấy cọc nguồn làm cọc trung gian
-		move(n - 1, b, a, c);
-	}
-
-	static void towerHanoi(int n) {
-		move(n, "A", "B", "C");
-	}
-
-	public static void main(String[] args) {
-		int n = 3;
-		towerHanoi(n);
-		System.out.println("Total: " + total);
-	}
+    public static void main(String[] args) {
+        int n = 3;
+        solveHanoi(n, 'A', 'C', 'B');
+    }
 }
