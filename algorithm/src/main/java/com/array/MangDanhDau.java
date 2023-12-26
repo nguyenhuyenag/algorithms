be4db44356f384f9729f40fbf358444fcc150426
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-12-26 23:07:20 +0700

    #

--- algorithm/src/main/java/com/array/MangDanhDau.java
+++ algorithm/src/main/java/com/array/MangDanhDau.java
@@ -2,11 +2,9 @@ package com.array;
 
 public class MangDanhDau {
 
-    /**
-     * Mảng để đếm tần suất xuất hiện của các ký tự ASCII:
-     *
+    /*-
+     * Mảng các ký tự ASCII:
      * Nếu c = 'a', giá trị ASCII của 'a' là 97. Do đó, charCount[97] sẽ được tăng lên 1.
-     *
      * Nếu c = 'A', giá trị ASCII của 'A' là 65. Do đó, charCount[65] sẽ được tăng lên 1.
      */
     public static void ascii(String input) {
@@ -25,35 +23,61 @@ public class MangDanhDau {
     }
 
     /*-
-     *      'a' - 'a' = 0
-     *      'b' - 'a' = 1
-     *      'c' - 'a' = 2
-     *
-     *    => currentChar - 'a' là chỉ số của 'a'
+     * Mảng ký tự thường
+     *  'a' - 'a' = 0
+     *  'b' - 'a' = 1
+     *  'c' - 'a' = 2
+     * => currentChar - 'a' là chỉ số của 'a'
      */
-    public void lowercase() {
-        String inputString = "abcabcxyz"; // Chuỗi đầu vào
-        // Mảng đánh dấu
-        boolean[] visited = new boolean[26]; // Giả sử chỉ xét các ký tự từ 'a' đến 'z'
-        System.out.println("Chuoi dau vao: " + inputString);
-        for (int i = 0; i < inputString.length(); i++) {
-            char currentChar = inputString.charAt(i);
-            if ('a' <= currentChar && currentChar <= 'c') {
-                visited[currentChar - 'a'] = true;
+    public static void lowercase() {
+        String input = "abcxyz";
+        boolean[] mark = new boolean[26]; // 'a' đến 'z' là 26 ký tự
+        // System.out.println("Input: " + input);
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isLowerCase(c)) { // 'a' <= c && c <= 'c'
+                mark[c - 'a'] = true;
             }
         }
+
         // In kết quả đánh dấu
-        System.out.print("Cac ky tu a, b, c da duoc su dung: ");
-        for (int i = 0; i < visited.length; i++) {
-            if (visited[i]) {
-                char currentChar = (char) ('a' + i);
+        System.out.print("Cac ky tu da duoc su dung: ");
+        for (int i = 0; i < mark.length; i++) {
+            if (mark[i]) {
+                char c = (char) ('a' + i);
+                System.out.print(c + " ");
+            }
+        }
+    }
+
+    public static void letter() {
+        String input = "HelloWorld123";
+        // Mảng đánh dấu cho ký tự 'a' đến 'z' và 'A' đến 'Z'
+        boolean[] charUsed = new boolean[52];
+        System.out.println("Chuoi dau vao: " + input);
+        // Duyệt qua chuỗi và đánh dấu các ký tự đã sử dụng
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isLowerCase(c)) {
+                charUsed[c - 'a'] = true;
+            } else if (Character.isUpperCase(c)) {
+                charUsed[c - 'A' + 26] = true;
+            }
+        }
+
+        // In kết quả đánh dấu
+        System.out.print("Cac ky tu a - z, A - Z da duoc su dung: ");
+        for (int i = 0; i < charUsed.length; i++) {
+            if (charUsed[i]) {
+                char currentChar = (i < 26) ? (char) ('a' + i) : (char) ('A' + i - 26);
                 System.out.print(currentChar + " ");
             }
         }
     }
 
     public static void main(String[] args) {
-        ascii("(( @");
+        // ascii("(( @");
+        lowercase();
     }
 
 }
--- codewars/src/main/java/codewars/WarMain.java
+++ codewars/src/main/java/codewars/WarMain.java
@@ -4,45 +4,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/*-
- * https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/
- *
- * Mã hóa chuỗi, nếu xuất hiện 1 lần là '(', từ 2 lần thì là ')'
- *
- *      "din"      =>  "((("
- *      "recede"   =>  "()()()"
- *      "Success"  =>  ")())())"
- *      "(( @"     =>  "))(("
+/**
+ * https://www.codewars.com/kata/5839edaa6754d6fec10000a2/
  */
 public class WarMain {
 
-    public static String encode(String word) {
-        word = word.toLowerCase();
-        int[] charCount = new int[256];
-        for (char c : word.toCharArray()) {
-            charCount[c]++;
-        }
-        StringBuilder ans = new StringBuilder();
-        for (char c : word.toCharArray()) {
-            if (charCount[c] > 0) {
-                if (charCount[c] == 1) {
-                    ans.append("(");
-                } else {
-                    ans.append(")");
-                }
-            }
-        }
-        return ans.toString();
+    public static char findMissingLetter(char[] array) {
+        int[] mark = new int[26];
+        return ' ';
     }
 
     @Test
     public void doTest() {
-        // assertEquals("(((", encode("din"));
-        // assertEquals("()()()", encode("recede"));
-        // assertEquals(")())())", encode("Success"));
-
-        // assertEquals(")()())()(()()(", encode("Prespecialized"));
-        assertEquals("))))())))", encode("   ()(   "));
+        assertEquals('e', findMissingLetter(new char[]{'a', 'b', 'c', 'd', 'f'}));
     }
 
 }
