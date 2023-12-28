a9f3949d76e7d70365373bb7c6f5ab3aa405069f
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-12-28 22:35:14 +0700

    #

--- codewars/src/main/java/codewars/WarMain.java
+++ codewars/src/main/java/codewars/WarMain.java
@@ -5,30 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * https://www.codewars.com/kata/514b92a657cdc65150000006/
- *
- * Liệt kê tất cả các số tự nhiên < 10 là bội số của 3 hoặc 5, chúng ta sẽ thu được 3, 5, 6 và 9. Tổng của những bội số này là 23.
- *
- * Giải bài toán trong trường hợp n
- *
- * Chú ý: Nếu số đó là bội số của cả 3 và 5, chỉ tính một lần, ví dụ số 15
+ * https://www.codewars.com/kata/5277c8a221e209d3f6000b56/
  */
 public class WarMain {
 
-    public int solution(int number) {
-        int sum = 0;
-        boolean[] mark = new boolean[number + 1];
-        for (int i = 0; i < number; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
-                sum += i;
-            }
-        }
-        return sum;
+    public boolean isValid(String braces) {
+        return false;
     }
 
     @Test
     public void doTest() {
-        assertEquals(23, solution(10));
+        // assertEquals(23, solution(10));
     }
 
 }
