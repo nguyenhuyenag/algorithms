db4ecdefd5558adbe4c035f8621ff3c00d1465b6
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-12-29 21:13:39 +0700

    #

--- algorithm/src/main/java/com/recursion/TowerHaNoi.java
+++ algorithm/src/main/java/com/recursion/TowerHaNoi.java
@@ -1,32 +1,53 @@
 package com.recursion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class TowerHaNoi {
 
     public static void main(String[] args) {
-        int n = 3;
-        Stack<Integer> A = new Stack<>();
-        Stack<Integer> B = new Stack<>();
-        Stack<Integer> C = new Stack<>();
+        List<List<Integer>> towers = new ArrayList<>();
+
+        int n = 3; // Number of discs
+
+        // Initialize the towers
+        for (int i = 0; i < 3; i++) {
+            towers.add(new ArrayList<>());
+        }
+
+        // Add discs to the first tower
         for (int i = n; i > 0; i--) {
-            A.push(i);
+            towers.get(0).add(i);
         }
-        printState(A, B, C);
-        move(n, A, B, C);
+
+        // Print initial state
+        printTowers(towers);
+
+        // Move discs from tower 1 to tower 3 using tower 2 as auxiliary
+        moveTower(n, towers, 0, 2, 1);
     }
 
-    public static void move(int n, Stack<Integer> A, Stack<Integer> B, Stack<Integer> C) {
+    private static void moveTower(int n, List<List<Integer>> towers, int A, int C, int B) {
         if (n > 0) {
-            move(n - 1, A, C, B);
-            C.push(A.pop());
-            printState(A, B, C);
-            move(n - 1, B, A, C);
+            // Move n-1 discs from source to auxiliary tower using destination tower as auxiliary
+            moveTower(n - 1, towers, A, B, C);
+
+            // Move the nth disc from source to destination
+            int disc = towers.get(A).remove(towers.get(A).size() - 1);
+            towers.get(C).add(disc);
+
+            // Print the current state of towers
+            printTowers(towers);
+
+            // Move the n-1 discs from auxiliary tower to destination using source tower as auxiliary
+            moveTower(n - 1, towers, B, C, A);
         }
     }
 
-    public static void printState(Stack<?> A, Stack<?> B, Stack<?> C) {
-        System.out.println(A + ", " + B + ", " + C);
+    private static void printTowers(List<List<Integer>> towers) {
+        System.out.println(towers);
     }
 
 }
