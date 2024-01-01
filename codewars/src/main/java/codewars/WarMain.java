bcc3192e15b395be8585c1aaec5b48c4dba94f33
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-01 21:30:32 +0700

    #

--- codewars/src/main/java/codewars/WarMain.java
+++ codewars/src/main/java/codewars/WarMain.java
@@ -2,75 +2,48 @@ package codewars;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
 
 /**
- * https://www.codewars.com/kata/52fb87703c1351ebd200081f/
+ * https://www.codewars.com/kata/5526fc09a1bbd946250002dc/
+ * 
+ * Tìm số ngoại lai:
+ * 
+ * [2, 4, 0, 100, 4, 11, 2602, 36] --> 11 (the only odd number)
+ * 
+ * [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
  */
 public class WarMain {
 
-	public static String whatCentury(int year) {
-		String suffix;
-		int century = (year - 1) / 100 + 1;
-		if (11 <= century % 100 && century % 100 <= 13) {
-			suffix = "th";
-		} else {
-			suffix = switch (century % 10) {
-				case 1 -> "st";
-				case 2 -> "nd";
-				case 3 -> "rd";
-				default -> "th";
-			};
+	public static int find(int[] nums) {
+		int len = nums.length;
+
+		boolean[] mark = new boolean[len];
+		for (int i = 0; i < len; i++) {
+			mark[i] = nums[i] % 2 == 0;
 		}
 
-		return century + suffix;
-	}
-	
-	@ParameterizedTest
-    @CsvSource({
-            "9720, 98th",
-            "6077, 61st",
-            "1435, 15th",
-            "3415, 35th",
-            "9548, 96th",
-            "2913, 30th",
-            "9156, 92nd",
-            "3876, 39th",
-            "8996, 90th",
-            "1855, 19th",
-            "4363, 44th",
-            "1307, 14th",
-            "7698, 77th",
-            "6024, 61st",
-            "8962, 90th",
-            "4551, 46th",
-            "6488, 65th",
-            "8361, 84th",
-            "9597, 96th",
-            "4963, 50th",
-            "8044, 81st",
-            "3842, 39th",
-            "4956, 50th",
-            "9444, 95th",
-            "9186, 92nd",
-            "6427, 65th",
-            "8573, 86th",
-            "4970, 50th",
-            "4105, 42nd",
-            "8163, 82nd",
-            "8566, 86th",
-            "3531, 36th",
-            "1135, 12th"
-    })
-    public void testWhatCentury(int year, String expected) {
-        assertEquals(expected, whatCentury(year));
-    }
+		System.out.println(Arrays.toString(mark));
 
-//	@ParameterizedTest
-//	@CsvFileSource(resources = "/your_test_data.csv", numLinesToSkip = 1)
-//	public void testYourProgram(int input, int expected, int actual) {
-//		assertEquals(expected, actual);
-//	}
+		boolean firstValue = mark[0];
+		for (int i = 1; i < len; i++) {
+			if (firstValue) { // Nếu firstValue = true
+				if (firstValue != mark[i]) {
+					return nums[i];
+				}
+			} else { // Nếu firstValue = false
+				
+			}
+		}
+		return 11;
+	}
+
+	@Test
+	public void doTest() {
+		assertEquals(11, find(new int[] { 2, 4, 0, 100, 4, 11, 2602, 36 }));
+		assertEquals(160, find(new int[] { 160, 3, 1719, 19, 11, 13, -21 }));
+	}
 
 }
--- leetcode/src/main/java/leetcode/LeetMain.java
+++ leetcode/src/main/java/leetcode/LeetMain.java
@@ -1,49 +1,14 @@
 package leetcode;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
 /**
- * Chủ đề: Two pointer
- * 
- * https://leetcode.com/problems/sum-of-square-numbers/
- * 
- * Kiểm tra số xem c = a^2 + b^2
  */
 public class LeetMain {
 
-//	public boolean judgeSquareSum0(long c) {
-//		for (long a = 0; a * a <= c; a++) {
-//			long bSquare = c - a * a;
-//			long b = (long) Math.sqrt(bSquare);
-//			if (b * b == bSquare) {
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
-
-	public boolean judgeSquareSum(int c) {
-		long a = 0;
-		long b = (long) Math.sqrt(c);
-		while (a <= b) {
-			long sum = a * a + b * b;
-			if (sum == c) {
-				return true;
-			} else 
-				if (sum < c) {
-					a++;
-				} else { // sum > c
-					b--;
-				}
-		}
-		return false;
-	}
-
 	@Test
 	public void testSomething() {
-		assertEquals(true, judgeSquareSum(25));
+		// assertEquals(10, findIt(new int[] { 10 }));
 	}
 
 }
