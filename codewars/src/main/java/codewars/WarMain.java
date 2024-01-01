186b8be8839ee86d404b4fb0dce034d0ccbdfa90
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-01 22:09:05 +0700

    #

--- codewars/src/main/java/codewars/WarMain.java
+++ codewars/src/main/java/codewars/WarMain.java
@@ -1,10 +1,7 @@
 package codewars;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Arrays;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * https://www.codewars.com/kata/5526fc09a1bbd946250002dc/
@@ -25,25 +22,42 @@ public class WarMain {
 			mark[i] = nums[i] % 2 == 0;
 		}
 
-		System.out.println(Arrays.toString(mark));
+		// System.out.println(Arrays.toString(mark));
 
-		boolean firstValue = mark[0];
-		for (int i = 1; i < len; i++) {
-			if (firstValue) { // Nếu firstValue = true
-				if (firstValue != mark[i]) {
-					return nums[i];
-				}
-			} else { // Nếu firstValue = false
-				
+		int countTrue = 0;
+		int countFalse = 0;
+		for (int i = 0; i < len; i++) {
+			if (mark[i]) {
+				countTrue++;
+			} else {
+				countFalse++;
 			}
 		}
-		return 11;
+		// System.out.println(countTrue + ", " + countFalse);
+		if (countTrue > 1) { // -> false là phần tử cần tìm
+			for (int i = 0; i < len; i++) {
+				if (!mark[i]) {
+					return nums[i];
+				}
+			}
+		}
+		if (countFalse > 1) { // -> true là phần tử cần tìm
+			for (int i = 0; i < len; i++) {
+				if (mark[i]) {
+					return nums[i];
+				}
+			}
+		}
+		return -1;
 	}
 
 	@Test
 	public void doTest() {
 		assertEquals(11, find(new int[] { 2, 4, 0, 100, 4, 11, 2602, 36 }));
 		assertEquals(160, find(new int[] { 160, 3, 1719, 19, 11, 13, -21 }));
+		assertEquals(3, find(new int[] { 2, 6, 8, -10, 3 }));
+		assertEquals(206847684, find(new int[] { 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 }));
+		assertEquals(0, find(new int[] { Integer.MAX_VALUE, 0, 1 }));
 	}
 
 }
