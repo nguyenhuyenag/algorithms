0b805c9910f1be6fd384560d460ba62d7f6871ba
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-04 13:14:21 +0700

    #

--- algorithm/src/main/java/com/number/NumberUtils.java
+++ algorithm/src/main/java/com/number/NumberUtils.java
@@ -1,128 +1,175 @@
 package com.number;
 
+import java.util.Arrays;
+
 public class NumberUtils {
 
-	/**
-	 * Thập phân => nhị phân
-	 */
-	public static String toBinary(int n) {
-		String s = "";
-		while (n != 0) {
-			s = n % 2 + s;
-			n = n / 2;
-		}
-		return s; // return Integer.toBinaryString(n);
-	}
+    /**
+     * Thập phân => nhị phân
+     */
+    public static String toBinary(int n) {
+        String s = "";
+        while (n != 0) {
+            s = n % 2 + s;
+            n = n / 2;
+        }
+        return s; // return Integer.toBinaryString(n);
+    }
 
-	public static String toBinary0(int sum) {
-		StringBuilder binary = new StringBuilder();
-		if (sum == 0) {
-			return "0";
-		}
-		while (sum > 0) {
-			int remainder = sum % 2;
-			binary.insert(0, remainder);
-			sum /= 2;
-		}
-		return binary.toString();
-	}
-	
-	/**
-	 * Nhị phân => thập phân
-	 */
-	public static int binaryToDecimal(String binary) {
-		return Integer.parseInt(binary, 2);
-	}
+    public static String toBinary0(int sum) {
+        StringBuilder binary = new StringBuilder();
+        if (sum == 0) {
+            return "0";
+        }
+        while (sum > 0) {
+            int remainder = sum % 2;
+            binary.insert(0, remainder);
+            sum /= 2;
+        }
+        return binary.toString();
+    }
 
-	/**
-	 * Tổng các chữ số
-	 */
-	public static int sumDigit(int n) {
-		int s = 0;
-		while (n != 0) {
-			s += n % 10;
-			n /= 10;
-		}
-		return s;
-	}
+    /**
+     * Nhị phân => thập phân
+     */
+    public static int binaryToDecimal(String binary) {
+        return Integer.parseInt(binary, 2);
+    }
 
-	/**
-	 * Chữ số đầu tiên
-	 */
-	public static int firstDigit(int n) {
-		while (n > 10) {
-			n /= 10;
-		}
-		return n;
-	}
+    /**
+     * Tổng các chữ số
+     */
+    public static int sumDigit(int n) {
+        int s = 0;
+        while (n != 0) {
+            s += n % 10;
+            n /= 10;
+        }
+        return s;
+    }
 
-	/**
-	 * Đảo ngược số
-	 */
-	public static int reverseNumber(int n) {
-		int rev = 0;
-		while (n > 0) {
-			// b = n % 10 => Lấy chữ số ra
-			// thêm vào hàng đơn vị
-			// ab = 10 * a + b
-			rev = rev * 10 + n % 10;
-			n = n / 10;
-		}
-		return rev;
-	}
+    /**
+     * Chữ số đầu tiên
+     */
+    public static int firstDigit(int n) {
+        while (n > 10) {
+            n /= 10;
+        }
+        return n;
+    }
 
-	/**
-	 * Kiểm tra các chữ số có phải tăng dần
-	 */
-	public static boolean increment(int n) {
-		int tmp, end = n % 10;
-		n /= 10;
-		while (n > 0) {
-			tmp = n % 10;
-			if (tmp > end) {
-				return false;
-			}
-			end = tmp;
-			n /= 10;
-		}
-		return true;
-	}
+    /**
+     * Đảo ngược số
+     */
+    public static int reverseNumber(int n) {
+        int rev = 0;
+        while (n > 0) {
+            // b = n % 10 => Lấy chữ số ra
+            // thêm vào hàng đơn vị
+            // ab = 10 * a + b
+            rev = rev * 10 + n % 10;
+            n = n / 10;
+        }
+        return rev;
+    }
 
-	/**
-	 * Ước chung lớn nhất: Euclid
-	 */
-	public static int gcd(int a, int b) {
-		while (a != b) {
-			if (a > b) {
-				a = a - b;
-			} else {
-				b = b - a;
-			}
-		}
-		return a; // hay b cũng được vì lúc này a = b
-	}
-	
-	// n là lũy thừa của k: k^i = n
-	public static boolean isPowerOfAny(int n, int k) {
-		if (n <= 0) {
-			return false;
-		}
-		while (n > 1) {
-			if (n % k == 0) {
-				n /= k;
-			} else {
-				return false;
-			}
-		}
-		return true;
-	}
+    /**
+     * Kiểm tra các chữ số có phải tăng dần
+     */
+    public static boolean increment(int n) {
+        int tmp, end = n % 10;
+        n /= 10;
+        while (n > 0) {
+            tmp = n % 10;
+            if (tmp > end) {
+                return false;
+            }
+            end = tmp;
+            n /= 10;
+        }
+        return true;
+    }
 
-	public void numberToArrayDigits() {
-		int number = 123; // -> [1, 2, 3]
-		int[] digits = Integer.toString(number)
-				.chars()
-				.map(Character::getNumericValue)
-				.toArray();
-	}
+    /**
+     * Ước chung lớn nhất: Euclid
+     */
+    public static int gcd(int a, int b) {
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        return a; // hay b cũng được vì lúc này a = b
+    }
+
+    // n là lũy thừa của k: k^i = n
+    public static boolean isPowerOfAny(int n, int k) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n > 1) {
+            if (n % k == 0) {
+                n /= k;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void numberToArrayDigits() {
+        int number = 123; // -> [1, 2, 3]
+        int[] digits = Integer.toString(number)
+                .chars()
+                .map(Character::getNumericValue)
+                .toArray();
+    }
+
+    /**
+     * Chia 1 số ra dạng 3x + 2y + z
+     *
+     * Mọi số nguyên > 1 đều có thể viết thành dạng 3x + 2y?
+     */
+    public static int[] splitNumberInto3x2y(int num) {
+        int x = 0, y = 0;
+        while (num > 0) {
+            // Ưu tiên cho 3
+            if (num - 3 == 0 || num - 3 >= 2) {
+                num = num - 3;
+                x++;
+            } else {
+                num = num - 2;
+                y++;
+            }
+        }
+        return new int[]{x, y, num}; // {x, y, 0} vì n = 0 thì mới thoát vòng lặp
+    }
+
+    public static int[] splitNumberAll(int num, int a, int b) {
+        int x = 0, y = 0;
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
+        while (num > min) {
+            if (num - max == 0 || num - max >= min) {
+                num = num - max;
+                x++;
+            } else {
+                num = num - min;
+                y++;
+            }
+        }
+        return new int[]{x, y, num};
+    }
+
+    public static void main(String[] args) {
+        int n = 19;
+        int a = 4, b = 3;
+        System.out.println("n = " + n);
+        int[] arr = splitNumberAll(n, a, b);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Test: " + (n == a * arr[0] + b * arr[1] + arr[2]));
+    }
 
 }
--- leetcode/src/main/java/leetcode/MainLeet.java
+++ leetcode/src/main/java/leetcode/MainLeet.java
@@ -2,63 +2,66 @@ package leetcode;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/
+ * https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/
+ *
+ * Cho 1 mảng số nguyên dương, có thể chọn một trong 2 phép toán sau:
+ *
+ * - Tìm 2 phần tử bằng nhau và xóa chúng ra khỏi mảng
+ *
+ * - Tìm 3 phần tử bằng nhau và xóa chúng ra khỏi mảng
+ *
+ * Trả về số lần thực hiện nhỏ nhất để mảng rỗng hoặc -1 nếu không thể
  */
 public class MainLeet {
 
-    public boolean isPossibleDivide(int[] nums, int k) {
-        List<List<Integer>> lists = splitArray(nums, k);
-        return lists != null;
-    }
-
-    public static List<List<Integer>> splitArray(int[] nums, int k) {
-        if (nums.length % k != 0) {
-            // System.out.println("The array cannot be divided into sets of size k");
-            return null;  // The array cannot be divided into sets of size k
-        }
-
-        Arrays.sort(nums);  // Sort the array
-
-        List<List<Integer>> results = new ArrayList<>();
-        Map<Integer, Integer> numFrequency = new HashMap<>();
-
-        for (int num : nums) {
-            numFrequency.put(num, numFrequency.getOrDefault(num, 0) + 1);
-        }
-
-        for (int num : nums) {
-            if (numFrequency.get(num) > 0) {
-                List<Integer> currentSet = new ArrayList<>();
-                for (int i = 0; i < k; i++) {
-                    int nextNum = num + i;
-
-                    if (numFrequency.getOrDefault(nextNum, 0) > 0) {
-                        currentSet.add(nextNum);
-                        numFrequency.put(nextNum, numFrequency.get(nextNum) - 1);
-                    } else {
-                        return null;  // The array cannot be divided into sets of size k
-                    }
-                }
-                results.add(currentSet);
+    public static int[] splitNumberInto3x2y(int number) {
+        int x = 0, y = 0;
+        while (number > 0) {
+            if (number - 3 == 0 || number - 3 >= 2) {
+                number = number - 3;
+                x++;
+            } else {
+                number = number - 2;
+                y++;
             }
         }
-        // System.out.println("result = " + Arrays.toString(results.toArray()));
-        return results;
+        return new int[]{x, y, number};
     }
+
+    public int minOperations(int[] nums) {
+        int step = 0;
+        Map<Integer, Integer> numCount = new HashMap<>();
+        for (int num : nums) {
+            numCount.put(num, numCount.getOrDefault(num, 0) + 1);
+        }
+        System.out.println("numCount = " + numCount);
+        for (int count : numCount.values()) {
+            if (count < 2) {
+                return -1;
+            }
+            int[] split = splitNumberInto3x2y(count);
+            if (split[2] > 0) {
+                return -1;
+            } else {
+                step += (split[0] + split[1]);
+            }
+        }
+        System.out.println("step: "+step);
+        return step;
+}
 
     @Test
     public void testSomething() {
-        // [1,2,3,4], [3,4,5,6]
-        assertEquals(true, isPossibleDivide(new int[]{1, 2, 3, 3, 4, 4, 5, 6}, 4));
-        // [1,2,3] , [2,3,4] , [3,4,5], [9,10,11]
-        assertEquals(true, isPossibleDivide(new int[]{3, 2, 1, 2, 3, 4, 3, 4, 5, 9, 10, 11}, 3));
-        // false
-        assertEquals(false, isPossibleDivide(new int[]{1, 2, 3, 4}, 3));
+        // assertEquals(4, minOperations(new int[]{2, 3, 3, 2, 2, 4, 2, 3, 4}));
+        // assertEquals(-1, minOperations(new int[]{2,1,2,2,3,3}));
+        // assertEquals(7, minOperations(new int[]{14, 12, 14, 14, 12, 14, 14, 12, 12, 12, 12, 14, 14, 12, 14, 14, 14, 12, 12}));
+        assertEquals(5, minOperations(new int[]{19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19}));
     }
 
 }
