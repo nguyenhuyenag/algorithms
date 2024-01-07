68362ed4bac51f02d16b31efd749cee372ff988f
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-07 21:23:52 +0700

    #

--- codewars/src/main/java/codewars/MainWar.java
+++ codewars/src/main/java/codewars/MainWar.java
@@ -16,24 +16,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class MainWar {
 
-    public static int minimumSteps(int[] numbers, int k) {
-        return 1;
+    public int adjacentElementsProduct(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length - 1; i++) {
+            max = Math.max(max, nums[i] * nums[i + 1]);
+        }
+        return max;
+    }
+
+    private void test(int expected, int actual) {
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void checkSmallArraySize() {
-        doTest(new int[]{4, 6, 3}, 7, 1);
-        doTest(new int[]{10, 9, 9, 8}, 17, 1);
-    }
+    public void fixedTests() {
+        test(40, adjacentElementsProduct(new int[]{5, 8}));
+        test(6, adjacentElementsProduct(new int[]{1, 2, 3}));
+        test(90, adjacentElementsProduct(new int[]{1, 5, 10, 9}));
+        test(48, adjacentElementsProduct(new int[]{4, 12, 3, 1, 5}));
+        test(6, adjacentElementsProduct(new int[]{5, 1, 2, 3, 1, 4}));
 
-    @Test
-    public void checkLargerArraySize() {
-        doTest(new int[]{8, 9, 4, 2}, 23, 3);
-        doTest(new int[]{19, 98, 69, 28, 75, 45, 17, 98, 67}, 464, 8);
-    }
+        test(21, adjacentElementsProduct(new int[]{3, 6, -2, -5, 7, 3}));
+        test(50, adjacentElementsProduct(new int[]{9, 5, 10, 2, 24, -1, -48}));
+        test(30, adjacentElementsProduct(new int[]{5, 6, -4, 2, 3, 2, -23}));
+        test(-14, adjacentElementsProduct(new int[]{-23, 4, -5, 99, -27, 329, -2, 7, -921}));
+        test(6, adjacentElementsProduct(new int[]{5, 1, 2, 3, 1, 4}));
 
-    private void doTest(int[] numbers, int k, int expected) {
-        assertEquals(expected, minimumSteps(numbers, k));
+        test(0, adjacentElementsProduct(new int[]{1, 0, 1, 0, 1000}));
+        test(6, adjacentElementsProduct(new int[]{1, 2, 3, 0}));
     }
 
 }
