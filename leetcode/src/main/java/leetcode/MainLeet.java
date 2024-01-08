a2e08892f8992a5f98a7e391455ed4f0c807b751
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-08 22:14:39 +0700

    #

--- leetcode/src/main/java/leetcode/MainLeet.java
+++ leetcode/src/main/java/leetcode/MainLeet.java
@@ -1,56 +1,30 @@
 package leetcode;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 
 /**
- * https://leetcode.com/problems/3sum/
+ * https://leetcode.com/problems/container-with-most-water/
  */
 public class MainLeet {
 
-    public List<List<Integer>> threeSum(int[] nums) {
-        int len = nums.length;
-        Set<List<Integer>> result = new HashSet<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < len; i++) {
-            int n1 = nums[i];
-            for (int j = 0; j < len; j++) {
-                if (i != j) {
-                    int n2 = nums[j];
-                    int n3 = -(n1 + n2);
-                    if (map.containsKey(n3)) {
-                        int k = map.get(n3);
-                        if (j != k && k != i) {
-                            List<Integer> ans = Arrays.asList(n1, n2, n3);
-                            Collections.sort(ans);
-                            result.add(ans);
-                        }
-                    }
-                }
-            }
-            if (!map.containsKey(nums[i])) {
-                map.put(nums[i], i);
-            }
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length-1;
+        while (l < r) {
+
         }
-        return new ArrayList<>(result);
+        return -1;
     }
 
     @Test
-    public void test_array_pass() {
-        // int[] arr = {-1, 0, 1, 2, -1, -4};
-        // int[] arr = {0, 1, 1};
-        int[] arr = {1, 2, -2, -1};
-        List<List<Integer>> lists = threeSum(arr);
-        System.out.println(Arrays.toString(lists.toArray()));
+    public void test() {
+        assertEquals(1, maxArea(new int[]{1, 1}));
+        assertEquals(49, maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
     }
-
-//    @Test
-//    public void test_array_pass() {
-//        List<String> actual = Arrays.asList("fee", "fi", "foe");
-//        List<String> expected = Arrays.asList("fee", "fi", "foe");
-//        assertIterableEquals(actual, expected);
-//    }
 
 }
