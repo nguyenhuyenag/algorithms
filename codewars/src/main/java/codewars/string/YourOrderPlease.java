eb2107b53d985461f21dc633ea5b960760d74e62
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-13 22:06:26 +0700

    #

--- codewars/src/main/java/codewars/string/YourOrderPlease.java
+++ codewars/src/main/java/codewars/string/YourOrderPlease.java
@@ -1,42 +1,34 @@
 package codewars.string;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// https://www.codewars.com/kata/55c45be3b2079eccff00010f
+/**
+ * https://www.codewars.com/kata/55c45be3b2079eccff00010f/
+ */
 public class YourOrderPlease {
 
-	public static String order(String words) {
-		StringBuilder str = new StringBuilder("");
-		String[] arr = words.split("\\s");
-		for (int i = 1; i <= 9; i++) {
-			for (String s : arr) {
-				if (s.contains(String.valueOf(i))) {
-					str.append(" " + s);
-					break;
-				}
-			}
-		}
-		return str.toString().trim();
-	}
+    public static String order(String str) {
+        List<String> words = Arrays.asList(str.split(" "));
+        Collections.sort(words, Comparator.comparingInt(s -> {
+            String intNumber = s.replaceAll("\\D", "");
+            return intNumber.isEmpty() ? 0 : Integer.parseInt(intNumber);
+        }));
+        return String.join(" ", words);
+    }
 
-	public static void main(String[] args) {
-		System.out.println(order("is2 Thi1s T4est 3a"));
-	}
-
-	@Test
-	public void test1() {
-		assertEquals(YourOrderPlease.order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est");
-	}
-
-	@Test
-	public void test2() {
-		assertEquals(YourOrderPlease.order("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople");
-	}
-
-	@Test
-	public void test3() {
-		assertEquals(YourOrderPlease.order("Empty input should return empty string"), "");
-	}
+    @Test
+    public void test() {
+        assertEquals("Thi1s is2 3a T4est", order("is2 Thi1s T4est 3a"));
+        assertEquals("Fo1r the2 g3ood 4of th5e pe6ople", order("4of Fo1r pe6ople g3ood th5e the2"));
+        assertEquals("", order(""));
+    }
 
 }
