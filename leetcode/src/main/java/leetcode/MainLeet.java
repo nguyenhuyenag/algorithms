fe9fbd59770013c4e78ce1850ec208e03b9d2b34
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-13 18:26:19 +0700

    #

--- leetcode/src/main/java/leetcode/MainLeet.java
+++ leetcode/src/main/java/leetcode/MainLeet.java
@@ -1,43 +1,105 @@
 package leetcode;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.*;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 
 /**
- * https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
+ *
  */
 public class MainLeet {
 
-    public int countCommonElements(char[] array1, char[] array2) {
-        Set<Character> set1 = new HashSet<>();
-        Set<Character> set2 = new HashSet<>();
-        for (int i = 0; i < array1.length; i++) {
-            set1.add(array1[i]);
-            set2.add(array2[i]);
+//    // Đảo vị trí qua đường chéo chính (i,j) <-> (j,i)
+//    public void transpose(int[][] matrix) {
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = i; j < matrix[0].length; j++) {
+//                int temp = matrix[i][j];
+//                matrix[i][j] = matrix[j][i];
+//                matrix[j][i] = temp;
+//            }
+//        }
+//    }
+//
+//    // Đào mảng (dòng)
+//    public void reverseRows(int[][] matrix) {
+//        int len = matrix.length;
+//        for (int i = 0; i < len; i++) {
+//            int l = 0, r = len - 1;
+//            while (l < r) {
+//                // (i, l) <-> (i, r)
+//                int temp = matrix[i][l];
+//                matrix[i][l] = matrix[i][r];
+//                matrix[i][r] = temp;
+//                l++;
+//                r--;
+//            }
+//        }
+//    }
+//
+//    public void rotate(int[][] matrix) {
+//        transpose(matrix);
+//        reverseRows(matrix);
+//    }
+//
+//    public void printMatrix(int[][] matrix) {
+//        for (int[] row : matrix) {
+//            for (int num : row) {
+//                System.out.print(num + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+
+    public void transpose(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = t;
+            }
         }
-        set1.retainAll(set2);
-        return set1.size();
     }
 
-    public int minSteps(String s, String t) {
-        int[] mark = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            mark[s.charAt(i) - 'a']++;
-            mark[t.charAt(i) - 'a']--;
+    public void reverseRows(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            int l = 0, r = matrix.length - 1;
+            while (l < r) {
+                int t = matrix[i][l];
+                matrix[i][l] = matrix[i][r];
+                matrix[i][r] = t;
+                l++;
+                r--;
+            }
         }
-        return Arrays.stream(mark).filter(v -> v > 0).sum();
+    }
+
+    public void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reverseRows(matrix);
     }
 
     @Test
     public void test() {
-        assertEquals(1, minSteps("bab", "aba"));
-        assertEquals(5, minSteps("leetcode", "practice"));
-        assertEquals(0, minSteps("anagram", "mangaar"));
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        rotate(matrix);
+
+        System.out.println("\nRotated Matrix:");
+        printMatrix(matrix);
     }
 
 }
