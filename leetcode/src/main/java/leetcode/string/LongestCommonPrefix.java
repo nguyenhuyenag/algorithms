75bd57bcc99af496097efeee7f4a7bd6474eecda
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-16 17:14:31 +0700

    #

--- leetcode/src/main/java/leetcode/string/LongestCommonPrefix.java
+++ leetcode/src/main/java/leetcode/string/LongestCommonPrefix.java
@@ -10,22 +10,6 @@ import static org.junit.jupiter.api.Assertions.*;
 // https://leetcode.com/problems/longest-common-prefix/
 public class LongestCommonPrefix {
 
-//	public static String longestCommonPrefix(String[] strs) {
-//		String str_0 = strs[0];
-//		int len = strs.length;
-//		StringBuilder sb = new StringBuilder();
-//		for (int i = 0; i < str_0.length(); i++) {
-//			sb.append(str_0.charAt(i));
-//			for (int j = 1; j < len; j++) {
-//				if (!strs[j].startsWith(sb.toString())) {
-//					sb.deleteCharAt(i);
-//					return sb.toString();
-//				}
-//			}
-//		}
-//		return sb.toString();
-//	}
-
     /**
      * Kiểm tra xem tất cả các chuỗi trong mảng arr có chứa cùng một ký tự ở vị trí cụ thể index không
      */
