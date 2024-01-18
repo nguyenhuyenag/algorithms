058dca27f3af0848ffb62ce17e30f69e710d13a2
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-18 21:23:03 +0700

    #

--- leetcode/src/main/java/leetcode/MainLeet.java
+++ leetcode/src/main/java/leetcode/MainLeet.java
@@ -9,44 +9,38 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * https://leetcode.com/problems/count-nice-pairs-in-an-array/
+ * https://leetcode.com/problems/search-a-2d-matrix/
  */
 public class MainLeet {
 
-//    public static void main(String[] args) {
-//        int k = 8;
-//        int[] arr = {1, 2, 3, 5, 6};
-//        subarraySum(arr, k);
+//    public boolean searchMatrix(int[][] matrix, int target) {
+//        int m = matrix.length;
+//        int n = matrix[0].length;
+//        // System.out.println(String.format("m = %s, n = %s", m, n));
+//        for (int i = 0; i < m; i++) {
+//            int l = 0, r = n - 1;
+//            // System.out.println(Arrays.toString(matrix[i]));
+//            while (l <= r) {
+//                int mid = l + (r - l) / 2;
+//                if (matrix[i][mid] == target) {
+//                    return true;
+//                } else if (matrix[i][mid] < target) {
+//                    l = mid + 1;
+//                } else {
+//                    r = mid - 1;
+//                }
+//            }
+//        }
+//        return false;
 //    }
-
-    public void printSubarray(int[] arr, int start, int end, int k) {
-        System.out.print("Subarray with sum " + k + ": ");
-        for (int i = start; i <= end; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
-
-    public int subarraySum(int[] arr, int k) {
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int currentSum = 0;
-            for (int j = i; j < arr.length; j++) {
-                currentSum += arr[j];
-                if (currentSum == k) {
-                    // printSubarray(arr, i, j, k);
-                    System.out.println(Arrays.toString(Arrays.copyOfRange(arr, i, j + 1)));
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    @Test
-    public void test() {
-        // assertEquals(2, subarraySum(new int[]{1, 1, 1}, 2));
-        assertEquals(2, subarraySum(new int[]{1, 2, 3}, 3));
-    }
+//
+//    @Test
+//    public void test() {
+//        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+//        // assertEquals(true, searchMatrix(matrix, 3));
+//        // assertEquals(false, searchMatrix(matrix, 61));
+//        // assertEquals(true, searchMatrix(new int[][]{{1}}, 1));
+//        assertEquals(true, searchMatrix(new int[][]{{1, 3}}, 3));
+//    }
 
 }
---
+++ leetcode/src/main/java/leetcode/array/SearchA2DMatrix.java
@@ -0,0 +1,43 @@
+package leetcode.array;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * https://leetcode.com/problems/search-a-2d-matrix/
+ */
+public class SearchA2DMatrix {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        // System.out.println(String.format("m = %s, n = %s", m, n));
+        for (int i = 0; i < m; i++) {
+            int l = 0, r = n - 1;
+            // System.out.println(Arrays.toString(matrix[i]));
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (matrix[i][mid] == target) {
+                    return true;
+                } else if (matrix[i][mid] < target) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Test
+    public void test() {
+        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+        // assertEquals(true, searchMatrix(matrix, 3));
+        // assertEquals(false, searchMatrix(matrix, 61));
+        // assertEquals(true, searchMatrix(new int[][]{{1}}, 1));
+        assertEquals(true, searchMatrix(new int[][]{{1, 3}}, 3));
+    }
+
+}
