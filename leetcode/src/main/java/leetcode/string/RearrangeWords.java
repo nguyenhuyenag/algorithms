4ec9f6477d8890ffaa780c8a60a201bce43ad05e
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-23 22:50:16 +0700

    #

--- leetcode/src/main/java/leetcode/string/RearrangeWords.java
+++ leetcode/src/main/java/leetcode/string/RearrangeWords.java
@@ -1,39 +1,31 @@
 package leetcode.string;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/*-
- * Given a sentence text (a sentence is a string of space-separated words) in
- * the following format:
- * 
- * - First letter is in upper case.
- * 
- * - Each word in text are separated by a single space.
- * 
- * Your task is to rearrange the words in text such that all words are
- * rearranged in an increasing order of their lengths. If two words have the
- * same length, arrange them in their original order. Return the new text
- * following the format shown above.
- * 
+/**
  * https://leetcode.com/problems/rearrange-words-in-a-sentence/
+ *
+ * Sắp xếp lại chuỗi theo độ dài và viết hoa chữ cái đầu tiên
  */
 public class RearrangeWords {
 
-	public static String arrangeWords(String text) {
+	public String arrangeWords(String text) {
 		String[] arr = text.toLowerCase().split(" ");
-		Arrays.sort(arr, (s1, s2) -> s1.length() - s2.length());
-		String str = String.join(" ", arr);
-		return str.substring(0, 1).toUpperCase() + str.substring(1);
+		// Arrays.sort(arr, (s1, s2) -> s1.length() - s2.length());
+		Arrays.sort(arr, Comparator.comparingInt(String::length));
+		String result = String.join(" ", arr);
+		return Character.toUpperCase(result.charAt(0)) + result.substring(1);
 	}
 
 	@Test
 	public void test() {
 		assertEquals("Is cool leetcode", arrangeWords("Leetcode is cool"));
-		assertEquals("On and keep calm code", arrangeWords("Keep calm and code on"));
 		assertEquals("To be or to be not", arrangeWords("To be or not to be"));
+		assertEquals("On and keep calm code", arrangeWords("Keep calm and code on"));
 	}
 
 }
--- leetcode/src/main/java/leetcode/string/RepeatedSubstringPattern.java
+++ leetcode/src/main/java/leetcode/string/RepeatedSubstringPattern.java
@@ -6,62 +6,66 @@ import org.junit.jupiter.api.Test;
 
 /**
  * https://leetcode.com/problems/repeated-substring-pattern/
- * 
- * VD: "ababab" -> ab{3}
+ *
+ * Kiểm tra chuỗi s có phải là chuỗi lặp của một chuỗi nào đó không
+ *
+ *      "ababab"    =    ab x 3   -> true
  */
 public class RepeatedSubstringPattern {
 
-	// Time Limit Exceeded
-	public boolean repeatedSubstringPattern0(String input) {
-		for (int i = 0; i < input.length() / 2; i++) {
-			String sub = input.substring(0, i + 1);
-			// System.out.println("i=" + i + ", " + "sub: " + sub);
-			String afterReplace = input.replaceAll(sub, "");
-			// System.out.println("afterReplace: " + afterReplace + ", length: " +
-			// afterReplace.length());
-			if (afterReplace.length() == 0) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public boolean repeatedSubstringPattern_OK(String input) {
-		int len = input.length();
-		// Chỉ lặp đến len/2, vì nếu vượt quá thì chuỗi mới sẽ dài hơn chuỗi gốc
-		for (int i = 1; i <= len / 2; i++) {
-			if (len % i == 0) {
-				String substring = input.substring(0, i);
-				StringBuilder repeated = new StringBuilder();
-				// Repeat the substring to check if it can construct the original string
-				for (int j = 0; j < len / i; j++) {
-					repeated.append(substring);
-				}
-				if (repeated.toString().equals(input)) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-//	public boolean repeatedSubstringPattern(String str) {
-//		if (str.length() == 1) {
-//			return false;
+    // Time Limit Exceeded
+//	public boolean repeatedSubstringPattern0(String input) {
+//		for (int i = 0; i < input.length() / 2; i++) {
+//			String sub = input.substring(0, i + 1);
+//			// System.out.println("i=" + i + ", " + "sub: " + sub);
+//			String afterReplace = input.replaceAll(sub, "");
+//			// System.out.println("afterReplace: " + afterReplace + ", length: " +
+//			// afterReplace.length());
+//			if (afterReplace.length() == 0) {
+//				return true;
+//			}
 //		}
-//		Map<Character, Integer> map = new HashMap<>();
-//		for (char key : str.toCharArray()) {
-//			map.compute(key, (character, count) -> count == null ? 1 : ++count);
-//		}
-//		Set<Integer> values = new HashSet<>(map.values());
-//		return values.size() == 1;
+//		return false;
 //	}
 
-	@Test
-	public void test() {
-		// assertEquals(false, repeatedSubstringPattern("aba"));
-		// assertEquals(true, repeatedSubstringPattern("ababab"));
-		assertEquals(false, repeatedSubstringPattern_OK("a"));
-	}
+//	public boolean repeatedSubstringPattern_OK(String input) {
+//		int len = input.length();
+//		// Chỉ lặp đến len/2, vì nếu vượt quá thì chuỗi mới sẽ dài hơn chuỗi gốc
+//		for (int i = 1; i <= len / 2; i++) {
+//			if (len % i == 0) {
+//				String substring = input.substring(0, i);
+//				StringBuilder repeated = new StringBuilder();
+//				// Repeat the substring to check if it can construct the original string
+//				for (int j = 0; j < len / i; j++) {
+//					repeated.append(substring);
+//				}
+//				if (repeated.toString().equals(input)) {
+//					return true;
+//				}
+//			}
+//		}
+//		return false;
+//	}
+
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        // Chỉ lặp đến n/2, vì nếu vượt quá thì chuỗi mới sẽ dài hơn chuỗi gốc
+        for (int i = 1; i <= n / 2; i++) {
+            //  Nếu n != k * i thì không cần lặp
+            if (n % i != 0) continue;
+            String sub = s.substring(0, i);
+            if (s.equals(sub.repeat(n / i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(false, repeatedSubstringPattern("a"));
+        assertEquals(false, repeatedSubstringPattern("aba"));
+        assertEquals(true, repeatedSubstringPattern("ababab"));
+    }
 
 }
