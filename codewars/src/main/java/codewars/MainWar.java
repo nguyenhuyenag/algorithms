43f9e2a8123a50ed9a20b559f8096cc2d0cc3664
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-24 22:30:12 +0700

    #

--- codewars/src/main/java/codewars/MainWar.java
+++ codewars/src/main/java/codewars/MainWar.java
@@ -14,9 +14,28 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
  */
 public class MainWar {
 
-    @Test
-    public void testMax() {
-        // assertEquals(8543210, max(23));
-    }
+//    public static int rowSumOddNumbers(int n) {
+//        int sum = 0;
+//        int currentNumber = 1;
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                if (i == n) {
+//                    // System.out.print((2 * currentNumber - 1) + " ");
+//                    sum += 2 * currentNumber - 1;
+//                }
+//                currentNumber++;
+//            }
+//            // System.out.println();
+//        }
+//        // System.out.println("sum = " + sum);
+//        return sum;
+//    }
+//
+//    @Test
+//    public void testMax() {
+//        assertEquals(1, rowSumOddNumbers(1));
+//        assertEquals(8, rowSumOddNumbers(2));
+//        assertEquals(27, rowSumOddNumbers(3));
+//    }
 
 }
