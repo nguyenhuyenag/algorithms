89d56a2cd6541ca1814e10548c888a8d555788e6
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-01-30 20:28:10 +0700

    #

--- algorithm/src/main/java/com/array/CacPhepToanTrenMang.java
+++ algorithm/src/main/java/com/array/CacPhepToanTrenMang.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * arraycopy(int[] src, int srcPos, int[] dest, int destPos, int length)
+ * System.arraycopy(int[] src, int srcPos, int[] dest, int destPos, int length)
  *
  *      src        Mảng nguồn
  *      srcPos     Vị trí bắt đầu sao chép
@@ -20,7 +20,7 @@ public class CacPhepToanTrenMang {
     /**
      * Gộp 2 mảng
      */
-    public void merge1(int[] A, int[] B) {
+    public void merge_0(int[] A, int[] B) {
         int lenA = A.length, lenB = B.length;
         int[] result = new int[lenA + lenB];
         System.arraycopy(A, 0, result, 0, lenA);
@@ -31,7 +31,7 @@ public class CacPhepToanTrenMang {
     /**
      * Gộp 2 mảng
      */
-    public int[] merge2(int[] A, int[] B) {
+    public int[] merge_1(int[] A, int[] B) {
         int pointer = 0;
         int[] result = new int[A.length + B.length];
         for (int x : A) {
@@ -44,9 +44,9 @@ public class CacPhepToanTrenMang {
     }
 
     /**
-     * Giao 2 mảng (tìm phần tử chung)
+     * Tìm phần tử chung của 2 mảng
      */
-    public int[] intersect(int[] A, int[] B) {
+    public List<Integer> intersect(int[] A, int[] B) {
         Arrays.sort(A);
         Arrays.sort(B);
         List<Integer> result = new ArrayList<>();
@@ -62,23 +62,23 @@ public class CacPhepToanTrenMang {
                 j++;
             }
         }
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return result;
     }
 
     @Test
     public void testIntersect() {
         // int[] arr = intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2});
-        int[] arr = intersect(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4});
-        System.out.println("arr = " + Arrays.toString(arr));
+        List<Integer> intersect = intersect(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4});
+        System.out.println(Arrays.toString(intersect.toArray()));
     }
 
     @Test
     public void testMerge() {
         int[] A = {1, 2, 3, 4};
         int[] B = {5, 6, 7};
-        int[] X = merge2(A, B);
-        System.out.println("X = " + Arrays.toString(X));
-        merge2(A, B);
+//        int[] X = merge2(A, B);
+//        System.out.println("X = " + Arrays.toString(X));
+//        merge2(A, B);
     }
 
 }
