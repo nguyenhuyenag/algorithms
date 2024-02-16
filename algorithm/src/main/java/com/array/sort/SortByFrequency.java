e527b714d7345dda23e3f6670221d0548274bc1c
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-02-16 13:19:58 +0700

    #

---
+++ algorithm/src/main/java/com/array/sort/SortByFrequency.java
@@ -0,0 +1,35 @@
+package com.array.sort;
+
+import java.util.*;
+
+/**
+ * Sắp xêp mảng theo số lần xuất hiện của các phần tử
+ */
+public class SortByFrequency {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 8, 5, 8, 4, 0, 0};
+        Map<Integer, Integer> numCount = new HashMap<>();
+        for (int v : arr) {
+            numCount.put(v, numCount.getOrDefault(v, 0) + 1);
+        }
+        // Sort 1
+        List<Integer> result1 = Arrays.stream(arr)
+                .boxed()
+                .sorted(Comparator.comparingInt(numCount::get))
+                .toList();
+        // Sort 2
+        List<Integer> result2 = Arrays.stream(arr)
+                .boxed()
+                .sorted((u, v) -> {
+                    int compareByFrequency = numCount.get(u) - numCount.get(v);
+                    return compareByFrequency != 0 ? compareByFrequency : u - v;
+                })
+                // .sorted(Comparator.comparingInt((Integer u) -> numCount.get(u)).thenComparingInt(u -> u))
+                .toList();
+        System.out.println("numCount = " + numCount);
+        System.out.println("result1 = " + Arrays.toString(result1.toArray()));
+        System.out.println("result2 = " + Arrays.toString(result2.toArray()));
+    }
+
+}
--- leetcode/src/main/java/leetcode/array/FindPolygonWithTheLargestPerimeter.java
+++ leetcode/src/main/java/leetcode/array/FindPolygonWithTheLargestPerimeter.java
@@ -11,31 +11,31 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * https://leetcode.com/problems/find-polygon-with-the-largest-perimeter/
  *
- * Các số a1, a2,... sẽ tạo thành 1 đa giác lồi nếu một số nhỏ hơn tổng của tất cả các số còn lại.
- * Cho mảng số nguyên dương, trả về chu vi lớn nhất có thể có được tạo các số trong mảng
+ * Các số a1, a2,... tạo thành 1 đa giác lồi nếu một số nhỏ hơn tổng của các số còn lại.
+ * Cho mảng số nguyên dương, trả về chu vi lớn nhất có thể tạo được các số trong mảng
+ *
+ * Ý tưởng: Duyệt mảng, nếu có phần tử không thỏa thì xóa nó ra khỏi mảng
  */
 public class FindPolygonWithTheLargestPerimeter {
 
     public long largestPerimeter(int[] nums) {
         // Chuyển thành List và đảo ngược
-        List<Integer> list = Arrays.stream(nums)
-                .boxed()
-                .sorted(Comparator.reverseOrder())
-                .collect(Collectors.toList());
+        List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
         // Tỉnh tổng các phần từ
         long p = list.stream().mapToLong(Integer::longValue).sum();
         while (list.size() > 2) {
-            int i;
+            boolean flag = true;
             // Duyệt list, nếu có phần từ nào mà > tất cả các số còn lại thì xóa nó ra khỏi list
-            for (i = 0; i < list.size(); i++) {
+            for (int i = 0; i < list.size(); i++) {
                 int x = list.get(i);
                 if (p - x <= x) {
                     p -= x;
                     list.remove(i);
+                    flag = false;
                     break;
                 }
             }
-            if (i == list.size()) return p;
+            if (flag) return p;
         }
         return -1;
     }
@@ -46,4 +46,29 @@ public class FindPolygonWithTheLargestPerimeter {
         assertEquals(-1, largestPerimeter(new int[]{5, 5, 50}));
         assertEquals(12, largestPerimeter(new int[]{1, 12, 1, 2, 5, 50, 3}));
     }
+
+    //    public long largestPerimeter_OK(int[] nums) {
+//        // Chuyển thành List và đảo ngược
+//        List<Integer> list = Arrays.stream(nums)
+//                .boxed()
+//                .sorted(Comparator.reverseOrder())
+//                .collect(Collectors.toList());
+//        // Tỉnh tổng các phần từ
+//        long p = list.stream().mapToLong(Integer::longValue).sum();
+//        while (list.size() > 2) {
+//            int i;
+//            // Duyệt list, nếu có phần từ nào mà > tất cả các số còn lại thì xóa nó ra khỏi list
+//            for (i = 0; i < list.size(); i++) {
+//                int x = list.get(i);
+//                if (p - x <= x) {
+//                    p -= x;
+//                    list.remove(i);
+//                    break;
+//                }
+//            }
+//            if (i == list.size()) return p;
+//        }
+//        return -1;
+//    }
+
 }
