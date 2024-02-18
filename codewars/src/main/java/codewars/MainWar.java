f278a22e36b73261085711c2e2e435bcae3afe68
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-02-18 09:52:24 +0700

    #

--- codewars/src/main/java/codewars/MainWar.java
+++ codewars/src/main/java/codewars/MainWar.java
@@ -1,9 +1,11 @@
 package codewars;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,9 +15,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class MainWar {
 
-    @Test
-    public void test() {
-        System.out.println(9 / 2);
+    public static int sum(int[] nums) {
+        if (nums == null || nums.length <= 1) return 0;
+        IntSummaryStatistics summary = Arrays.stream(nums).summaryStatistics();
+        return (int) (summary.getSum() - summary.getMin() - summary.getMax());
     }
 
 }
---
+++ codewars/src/main/java/codewars/array/RangeExtraction.java
@@ -0,0 +1,53 @@
+package codewars.array;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.StringJoiner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * https://www.codewars.com/kata/51ba717bb08c1cd60f00002f/
+ *
+ * Cho mảng số nguyên, mảng con tạo thành 1 dãy liên tục thì ghép thành "fist-last"
+ *
+ * Ví dụ: [-3, -2, -1, 2, 10, 15, 16, 18, 19, 20] -> "-3--1,2,10,15,16,18-20"
+ */
+public class RangeExtraction {
+
+    private static void createRange(LinkedList<Integer> list, StringJoiner result) {
+        int size = list.size();
+        if (size == 1) {
+            result.add(list.getFirst().toString());
+        } else if (size == 2) {
+            result.add(list.getFirst() + "," + list.getLast());
+        } else {
+            result.add(list.getFirst() + "-" + list.getLast());
+        }
+        list.clear();
+    }
+
+    public static String rangeExtraction(int[] arr) {
+        StringJoiner result = new StringJoiner(",");
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int x : arr) {
+            // Nếu số thêm mới không phải là số liền kề ở đình -> Ghép thành chuỗi
+            if (!list.isEmpty() && x != list.getLast() + 1) {
+                createRange(list, result);
+            }
+            list.add(x);
+        }
+        // Nếu list vẫn còn phần tử -> Ghép chuỗi
+        createRange(list, result);
+        return result.toString();
+    }
+
+    @Test
+    public void test_BasicTests() {
+        assertEquals("-3--1,2,10,15,16,18-20", rangeExtraction(new int[]{-3, -2, -1, 2, 10, 15, 16, 18, 19, 20}));
+        assertEquals("-6,-3-1,3-5,7-11,14,15,17-20", rangeExtraction(new int[]{-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20}));
+        assertEquals("-10--8,-6,-3-1,3-5,7-11,14,15,17-20", rangeExtraction(new int[]{-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20}));
+    }
+
+}
