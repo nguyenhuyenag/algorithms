a303ba39209138037f2e944ae130d2380e8760ed
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-02-24 11:40:14 +0700

    #

--- leetcode/src/main/java/leetcode/number/ReverseInteger.java
+++ leetcode/src/main/java/leetcode/number/ReverseInteger.java
@@ -14,7 +14,7 @@ public class ReverseInteger {
         return Long.parseLong(numStr.reverse().toString());
     }
 
-    public int reverse(int n) {
+    public int reverse_OK(int n) {
         long result = n > 0 ? helper(n) : -helper(-n);
         if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) {
             return 0;
@@ -22,11 +22,26 @@ public class ReverseInteger {
         return (int) result;
     }
 
+    public int reverse(int n) {
+        long result = 0;
+        int sign = n > 0 ? 1 : -1;
+        n = Math.abs(n);
+        while (n > 0) {
+            int digit = n % 10;
+            n /= 10;
+            result = 10 * result + digit;
+            if (sign * result < Integer.MIN_VALUE || sign * result > Integer.MAX_VALUE) {
+                return 0;
+            }
+        }
+        return (int) (sign * result);
+    }
+
     @Test
     public void test() {
-        assertEquals(0, reverse(0));
-        assertEquals(21, reverse(120));
-        assertEquals(321, reverse(123));
+        // assertEquals(0, reverse(0));
+        // assertEquals(21, reverse(120));
+        // assertEquals(321, reverse(123));
         assertEquals(-321, reverse(-123));
     }
 
