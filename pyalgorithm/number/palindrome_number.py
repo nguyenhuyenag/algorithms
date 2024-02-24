aa55e96851536cdab73a79e8554541ef7adff70a
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-02-24 12:15:48 +0700

    #

---
+++ pyalgorithm/number/palindrome_number.py
@@ -0,0 +1,34 @@
+import unittest
+
+"""
+    https://leetcode.com/problems/palindrome-number/
+    
+    Palindrome number là số mà đọc ngược và đọc xuôi đều bằng nhau
+"""
+
+
+class Solution(unittest.TestCase):
+
+    def isPalindrome_OK(self, x: int) -> bool:
+        if x < 0:
+            return False
+        return x == int(str(x)[::-1])
+
+    def isPalindrome(self, n: int) -> bool:
+        num = str(n)
+        left, right = 0, len(num) - 1
+        while left < right:
+            if num[left] != num[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
+    def test(self):
+        self.assertEqual(True, self.isPalindrome(11211))
+        self.assertEqual(False, self.isPalindrome(11213))
+        self.assertEqual(False, self.isPalindrome(-121))
+
+
+if __name__ == '__main__':
+    unittest.main()
