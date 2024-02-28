128a625675551a331f7e877ba573a556d018666a
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-02-28 21:19:33 +0700

    #

--- codewars/src/main/java/codewars/incomplete/TheSuperMarketQueue.java
+++ codewars/src/main/java/codewars/incomplete/TheSuperMarketQueue.java
@@ -1,5 +1,10 @@
 package codewars.incomplete;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * https://www.codewars.com/kata/57b06f90e298a7b53d000a86/
  *
@@ -11,12 +16,18 @@ package codewars.incomplete;
  * queueTime([5,3,4], 1) = 12    -> Vì chỉ có 1 quầy nên 5 + 3 + 4 = 12
  *
  * queueTime([10,2,3,3], 2) = 10 -> Người thứ nhất ở quầy 1, ba người còn lại ở quầy 2
- *                                  nên chỉ cần 10 phút
+ * nên chỉ cần 10 phút
  */
 public class TheSuperMarketQueue {
 
     public static int solveSuperMarketQueue(int[] customers, int n) {
         return 0;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(12, solveSuperMarketQueue(new int[]{5, 3, 4}, 1));
+        assertEquals(10, solveSuperMarketQueue(new int[]{10, 2, 3, 3}, 2));
     }
 
 }
