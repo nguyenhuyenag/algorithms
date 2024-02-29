8b213e7beadac5543ec0d76e6312c37a0788f03f
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-02-29 17:17:57 +0700

    #

---
+++ py-algorithm/string/so_many_permutations.py
@@ -0,0 +1,17 @@
+import unittest
+from typing import List
+
+"""
+    https://www.codewars.com/kata/5254ca2719453dcc0b00027d/
+"""
+class Solution(unittest.TestCase):
+
+    def doTest(self, nums: List[int]) -> bool:
+        pass
+
+    def test(self):
+        self.assertEqual(True, self.doTest([1, 1, 2, 3, 4]))
+
+
+if __name__ == '__main__':
+    unittest.main()
