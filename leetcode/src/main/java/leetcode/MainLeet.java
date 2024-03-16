62da77a2ed6264db4eaac1083b578d3d34fc2c3d
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-03-16 21:00:25 +0700

    #

--- leetcode/src/main/java/leetcode/MainLeet.java
+++ leetcode/src/main/java/leetcode/MainLeet.java
@@ -15,16 +15,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class MainLeet {
 
-    public static void main(String[] args) {
-        String s = "";
-        int[] mark1 = new int[26];
-        int[] mark2 = new int[26];
-        s.chars().forEach(c -> {
-            mark1[c - 'a']++;
-            mark2[c - 'a']++;
-        });
-        System.out.println(Arrays.hashCode(mark1));
-        System.out.println(Arrays.hashCode(mark2));
+    public int findMaxLength(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> mp = new HashMap<>();
+        int sum = 0;
+        int subArrayLength = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i] == 0 ? -1 : 1;
+            if (sum == 0) {
+                subArrayLength = i + 1;
+            } else if (mp.containsKey(sum)) {
+                subArrayLength = Math.max(subArrayLength, i - mp.get(sum));
+            } else {
+                mp.put(sum, i);
+            }
+        }
+        return subArrayLength;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(2, findMaxLength(new int[]{0, 1}));
+        // assertEquals(2, findMaxLength(new int[]{0, 1, 0}));
     }
 
 }
