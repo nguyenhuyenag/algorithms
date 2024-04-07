28d2aab413a20b373923e9fb7db9a13d0b2e4da9
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-04-07 08:35:29 +0700

    #

--- leetcode/src/main/java/leetcode/MainLeet.java
+++ leetcode/src/main/java/leetcode/MainLeet.java
@@ -1,24 +1,61 @@
 package leetcode;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+ */
 public class MainLeet {
 
-    @Test
-    public void test() {
-        Stack<Character> stack = new Stack<>();
-        stack.push('A');
-        stack.push('B');
-        stack.push('C');
-        stack.forEach(System.out::println);
-        Collections.reverse(stack);
-        stack.forEach(System.out::println);
-    }
+//    public String minRemoveToMakeValid(String s) {
+//        Stack<Integer> stack = new Stack<>();
+//        Set<Integer> invalidIndices = new HashSet<>();
+//
+//        // Bước 1: Tìm các dấu ngoặc không hợp lệ và lưu trữ chỉ mục của chúng
+//        for (int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//            if (c == '(') {
+//                stack.push(i);
+//            } else if (c == ')') {
+//                if (stack.isEmpty()) {
+//                    invalidIndices.add(i);
+//                } else {
+//                    stack.pop();
+//                }
+//            }
+//        }
+//
+//        // Đánh dấu các dấu ngoặc mở không có cặp là không hợp lệ
+//        while (!stack.isEmpty()) {
+//            invalidIndices.add(stack.pop());
+//        }
+//
+//        // Bước 2: Xây dựng chuỗi kết quả, bỏ qua các chỉ mục không hợp lệ
+//        StringBuilder result = new StringBuilder();
+//        for (int i = 0; i < s.length(); i++) {
+//            if (!invalidIndices.contains(i)) {
+//                result.append(s.charAt(i));
+//            }
+//        }
+//
+//        return result.toString();
+//    }
+//
+//    @Test
+//    public void test() {
+//        // assertEquals("lee(t(c)o)de", minRemoveToMakeValid("lee(t(c)o)de)"));
+//        // assertEquals("ab(c)d", minRemoveToMakeValid("a)b(c)d"));
+//        assertEquals("", minRemoveToMakeValid("))(("));
+//    }
 
 }
---
+++ leetcode/src/main/java/leetcode/incomplete/MinimumRemoveToMakeValidParentheses.java
@@ -0,0 +1,62 @@
+package leetcode.incomplete;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+ *
+ * Cho chuỗi ngẫu nhiên có chứa các dấu ngoặc '(', ')'. Xóa (ít nhất) các dấu
+ * ngoặc dư để chuỗi cuối cùng chỉ chứa các dấu ngoặc hợp lệ.
+ *
+ * Chuỗi rỗng luôn là chuỗi đúng.
+ */
+public class MinimumRemoveToMakeValidParentheses {
+
+    public String minRemoveToMakeValid(String s) {
+        Stack<Integer> stack = new Stack<>();
+        Set<Integer> invalidIndices = new HashSet<>();
+
+        // Bước 1: Tìm các dấu ngoặc không hợp lệ và index của chúng
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            } else if (c == ')') {
+                if (stack.isEmpty()) {
+                    invalidIndices.add(i);
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        // Đánh dấu các dấu ngoặc mở không có cặp là không hợp lệ
+        while (!stack.isEmpty()) {
+            invalidIndices.add(stack.pop());
+        }
+
+        // Bước 2: Xây dựng chuỗi kết quả, bỏ qua các chỉ mục không hợp lệ
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!invalidIndices.contains(i)) {
+                result.append(s.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+
+    @Test
+    public void test() {
+        // assertEquals("lee(t(c)o)de", minRemoveToMakeValid("lee(t(c)o)de)"));
+        // assertEquals("ab(c)d", minRemoveToMakeValid("a)b(c)d"));
+        assertEquals("", minRemoveToMakeValid("))(("));
+    }
+
+}
