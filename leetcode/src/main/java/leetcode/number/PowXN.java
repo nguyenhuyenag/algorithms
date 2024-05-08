01f70d97f6d6dc37d8a29b3d91b1903e5982a7e1
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-05-08 16:05:23 +0700

    #

--- leetcode/src/main/java/leetcode/number/PowXN.java
+++ leetcode/src/main/java/leetcode/number/PowXN.java
@@ -1,6 +1,5 @@
 package leetcode.number;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.Math.*;
@@ -9,22 +8,36 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * https://leetcode.com/problems/powx-n/
  *
- * Implement hàm: Math.pow(x, n)
+ * Implement hàm: pow(x, n)
  */
 public class PowXN {
 
-    public double myPow(double x, long n) {
+    public double myPow_OK(double x, long n) {
         if (n == 0) return 1;
         // n < 0
         if (n < 0) {
-            return 1 / myPow(x, -n);
+            return 1 / myPow_OK(x, -n);
         }
         // n > 0
         if (n % 2 == 0) {
-            double t = myPow(x, n / 2);
+            double t = myPow_OK(x, n / 2);
             return t * t;
         }
-        return x * myPow(x, n - 1);
+        return x * myPow_OK(x, n - 1);
+    }
+
+    public double myPow(double a, long n) {
+        if (n == 0) return 1;
+        if (n < 0) {
+            n = -n;
+            a = 1 / a;
+        }
+        if (n % 2 == 0) {
+            double temp = myPow(a, n / 2);
+            return temp * temp;
+        }
+        double temp = myPow(a, (n - 1) / 2);
+        return a * temp * temp;
     }
 
     @Test
@@ -34,7 +47,7 @@ public class PowXN {
         assertEquals(pow(2.1, 3), myPow(2.1, 3));
 
         // Time Limit Exceeded ↓
-        assertEquals(pow(1.0, 2147483647), myPow(1.00000, 2147483647));
+        assertEquals(pow(1.0, 2147483647), myPow_OK(1.00000, 2147483647));
     }
 
 //    	public static double myPow(double x, long n) {
