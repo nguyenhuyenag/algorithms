128563f7588e36647861b9e09dd3699f47dfa334
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-06-02 10:14:11 +0700

    #

--- algorithm/src/main/java/string/ReverseString.java
+++ algorithm/src/main/java/string/ReverseString.java
@@ -1,36 +1,61 @@
 package string;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/*
+	https://leetcode.com/problems/reverse-string/
+ */
 public class ReverseString {
 
-	public static String reverseString0(String str) {
-		return new StringBuilder(str).reverse().toString();
-	}
+//    public String reverseString_0(String str) {
+//        return new StringBuilder(str).reverse().toString();
+//    }
+//
+//    public String reverseString(String str) {
+//        if (str.isEmpty()) {
+//            return str;
+//        }
+//        return reverseString(str.substring(1)) + str.charAt(0);
+//    }
+//
+//    public String reverseString_2(String s) {
+//        char[] chars = s.toCharArray();
+//        int left = 0;
+//        int right = chars.length - 1;
+//        while (left < right) {
+//            char tmp = chars[left];
+//            chars[left] = chars[right];
+//            chars[right] = tmp;
+//            left++;
+//            right--;
+//        }
+//        return new String(chars);
+//    }
 
-	public static String reverseString1(String str) {
-		if (str.isEmpty()) {
-			return str;
+    public void reverseString_0(char[] arr) {
+		int len = arr.length;
+		for (int i = 0; i < len / 2; i++) {
+            char tmp = arr[i];
+            arr[i] = arr[(len - 1) - i];
+            arr[(len - 1) - i] = tmp;
+        }
+    }
+
+	public void reverseString(char[] arr) {
+		int l = 0, r = arr.length -1;
+		while (l < r) {
+			char tmp = arr[l];
+			arr[l] = arr[r];
+			arr[r] = tmp;
+			l++;
+			r--;
 		}
-		return reverseString1(str.substring(1)) + str.charAt(0);
 	}
 
-	public static String reverseString2(String original) {
-		char[] chars = original.toCharArray();
-		int left = 0;
-		int right = chars.length - 1;
-		while (left < right) {
-			char tmp = chars[left];
-			chars[left] = chars[right];
-			chars[right] = tmp;
-			left++;
-			right--;
-		}
-		return new String(chars);
-	}
-
-	public static void main(String[] args) {
-		String str = "Java";
-		System.out.println(reverseString1(str));
-		System.out.println(reverseString2(str));
-		System.out.println(new StringBuilder(str).reverse());
-	}
+    @Test
+    public void tets() {
+        String str = "Java";
+		reverseString("Java".toCharArray());
+    }
 }
