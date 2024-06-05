d86c12e2868c698f0e86734b79f1500a1b62a10f
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-06-05 17:14:10 +0700

    #

--- leetcode/src/main/java/test/MainLeet.java
+++ leetcode/src/main/java/test/MainLeet.java
@@ -14,9 +14,25 @@ import java.util.Arrays;
  */
 public class MainLeet {
 
-    public int[] constructArray(int n, int k) {
-        int[] result = new int[n + 1];
-        return result;
+    public int maximumSwap(int num) {
+        int max_digit = 0;
+        int d = 0;
+        int temp = 1, index = 1;
+        int n = num;
+
+        while (n > 0) {
+            int digit = n % 10;
+            n = n / 10;
+            if (max_digit > digit) {
+                d = Math.max(d, (max_digit - digit) * (temp - index));
+            } else if (max_digit < digit) {
+                max_digit = digit;
+                index = temp;
+            }
+            temp = temp * 10;
+        }
+
+        return num + d;
     }
 
     @Test
