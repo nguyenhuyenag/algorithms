d097dbcc5935c23d5e422eaac1326dab6c3125ba
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-06-25 14:19:34 +0700

    #

--- algorithm/src/main/java/array/FillArray.java
+++ algorithm/src/main/java/array/FillArray.java
@@ -2,68 +2,45 @@ package array;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class FillArray {
 
-//    public void fill2D(int[][] matrix, int value) {
-//        int m = matrix.length;
-//        int n = matrix[0].length;
-//        for (int i = 0; i < m * n; i++) {
-//            int row = i / n;
-//            int col = i % n;
-//            matrix[row][col] = value;
-//        }
-//    }
-//
-//    @Test
-//    public void test() {
-//        int[][] matrix = new int[5][6];
-//        fill2D(matrix, -1);
-//        for (int[] row : matrix) {
-//            System.out.println(Arrays.toString(row));
-//        }
-//    }
+    private final int M = 5;
+    private final int N = 5;
+    private final int[][] matrix = new int[M][N];
 
-    @Test
-    public void test() {
-        // Khởi tạo ma trận 2D
-        int m = 10_000; // số hàng
-        int n = 10_000; // số cột
-        int[][] matrix = new int[m][n];
-        int value = 5; // giá trị cần gán
-
-        // Đo thời gian cho phương thức fill2D
-        long startTime1 = System.currentTimeMillis();
-        fill2D(matrix, value);
-        long endTime1 = System.currentTimeMillis();
-        long duration1 = endTime1 - startTime1;
-        System.out.println("Thời gian thực thi của fill2D: " + duration1 + " milliseconds");
-
-        // Đo thời gian cho phương thức fill2DWithNestedLoops
-        long startTime2 = System.currentTimeMillis();
-        fill2DWithNestedLoops(matrix, value);
-        long endTime2 = System.currentTimeMillis();
-        long duration2 = endTime2 - startTime2;
-        System.out.println("Thời gian thực thi của fill2DWithNestedLoops: " + duration2 + " milliseconds");
-    }
-
-    public void fill2D(int[][] matrix, int value) {
+    public void test1() {
         int m = matrix.length;
         int n = matrix[0].length;
         for (int i = 0; i < m * n; i++) {
             int row = i / n;
             int col = i % n;
-            matrix[row][col] = value;
+            matrix[row][col] = -1;
         }
     }
 
-    public void fill2DWithNestedLoops(int[][] matrix, int value) {
+    public void test2() {
         int m = matrix.length;
         int n = matrix[0].length;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                matrix[i][j] = value;
+                matrix[i][j] = -1;
             }
         }
+    }
+
+    public void test3() {
+        for (int[] row : matrix) {
+            Arrays.fill(row, -1);
+        }
+    }
+
+    @Test
+    public void test() {
+        test1();
+        // test2();
+        // test3();
     }
 
 }
