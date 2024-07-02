6b670894b5b74403dbe8be6cdf9c42539f209625
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-07-02 16:43:51 +0700

    #

--- leetcode/src/main/java/array/IntersectionOfTwoArrayII.java
+++ leetcode/src/main/java/array/IntersectionOfTwoArrayII.java
@@ -1,11 +1,10 @@
 package array;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class IntersectionOfTwoArrayII {
 
-    public int[] intersect(int[] nums1, int[] nums2) {
+    public int[] intersect_1(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         List<Integer> result = new ArrayList<>();
@@ -35,12 +34,38 @@ public class IntersectionOfTwoArrayII {
         return result.stream().mapToInt(Integer::intValue).toArray();
     }
 
+    // Sử dụng map counter
+    public int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> counter = new HashMap<>();
+
+        for (int num : nums1) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : nums2) {
+            int v = counter.getOrDefault(num, 0);
+            if (v > 0) {
+                result.add(num);
+                counter.put(num, v - 1);
+            }
+        }
+
+        return result.stream()
+                .sorted() // sort để test, thực tế không cần sort
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+
+    @Disabled
     @Test
-    public void test() {
-        // int[] arr = intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2});
-        int[] arr = intersect(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4});
-        // System.out.println("arr = " + Arrays.toString(arr));
+    public void test1() {
         assertArrayEquals(new int[]{2, 2}, intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2}));
+    }
+
+    @Test
+    public void test2() {
+        assertArrayEquals(new int[]{4, 9}, intersect(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4}));
     }
 
 }
