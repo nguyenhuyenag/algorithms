fc0e287e1c8e4b167bb4db5bfcaaeadc727c8c39
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-08-27 09:25:03 +0700

    #

--- leetcode/src/main/java/array/DivisibleAndNonDivisibleSum.java
+++ leetcode/src/main/java/array/DivisibleAndNonDivisibleSum.java
@@ -1,15 +1,25 @@
-package test;
+package array;
 
 import org.junit.jupiter.api.Test;
 
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /*
+    https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/
+
+    You are given positive integers n and m.
+
+    Define two integers, num1 and num2, as follows:
+
+        num1: The sum of all integers in the range [1, n] that are not divisible by m.
+
+        num2: The sum of all integers in the range [1, n] that are divisible by m.
+
+    Return the integer num1 - num2.
  */
-public class MainLeet {
+public class DivisibleAndNonDivisibleSum {
 
     public int differenceOfSums(int n, int m) {
         int num1 = IntStream.rangeClosed(1, n)
