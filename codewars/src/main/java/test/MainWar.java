a8abd5a02b9d8e9c624eaa1ed461c5001de61931
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-08-30 15:55:22 +0700

    #

--- codewars/src/main/java/test/MainWar.java
+++ codewars/src/main/java/test/MainWar.java
@@ -1,27 +1,14 @@
 package test;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 /*
-    https://www.codewars.com/kata/51e056fe544cf36c410000fb/
  */
 public class MainWar {
 
-    public static int arithmetic(int a, int b, String operator) {
-        return switch (operator) {
-            case "add" -> a + b;
-            case "subtract" -> a - b;
-            case "multiply" -> a * b;
-            case "divide" -> a / b;
-            default -> throw new IllegalStateException("Unexpected value: " + operator);
-        };
-    }
-
     @Test
-    void testNotPowersOfTwo() {
+    public void test() {
+
     }
 
 }
