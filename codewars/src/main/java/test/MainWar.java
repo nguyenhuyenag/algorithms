766af3c11e920d8227d0360225706711ffc5133c
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-09-10 15:13:37 +0700

    #

--- codewars/src/main/java/test/MainWar.java
+++ codewars/src/main/java/test/MainWar.java
@@ -2,9 +2,22 @@ package test;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
 /*
  */
 public class MainWar {
+
+    public static String shortcut(String input) {
+        Set<Character> vowels = Set.of('a', 'e', 'o', 'u', 'i');
+        StringBuilder result = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            if (!vowels.contains(c)) {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
 
     @Test
     public void test() {
