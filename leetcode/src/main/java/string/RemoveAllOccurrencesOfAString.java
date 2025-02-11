8d60d11a4f4b9e4130411b24a1952dfcb718cb23
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2025-02-11 08:46:18 +0700

    #

---
+++ leetcode/src/main/java/string/RemoveAllOccurrencesOfAString.java
@@ -0,0 +1,27 @@
+package string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+    https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+ */
+public class RemoveAllOccurrencesOfAString {
+
+    public String removeOccurrences(String s, String part) {
+        int idx;
+        StringBuilder builder = new StringBuilder(s);
+        while ((idx = builder.indexOf(part)) != -1) {
+            builder.delete(idx, idx + part.length()); // Xóa phần tử được tìm thấy
+        }
+        return builder.toString();
+    }
+
+    @Test
+    public void test() {
+        assertEquals("dab", removeOccurrences("daabcbaabcbc", "abc"));
+        assertEquals("ab", removeOccurrences("axxxxyyyyb", "xy"));
+    }
+
+}
---
+++ leetcode/src/main/java/test/MainLeet.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+    https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+ */
+public class MainLeet {
+
+//    public String removeOccurrences(String s, String part) {
+//        int idx;
+//        StringBuilder builder = new StringBuilder(s);
+//        while ((idx = builder.indexOf(part)) != -1) {
+//            builder.delete(idx, idx + part.length()); // Xóa phần tử được tìm thấy
+//        }
+//        return builder.toString();
+//    }
+
+    @Test
+    public void test() {
+//        assertEquals("dab", removeOccurrences("daabcbaabcbc", "abc"));
+//        assertEquals("ab", removeOccurrences("axxxxyyyyb", "xy"));
+    }
+
+}
