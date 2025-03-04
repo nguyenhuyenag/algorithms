2a851885faa78182571518e706d67ead148ded46
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2025-03-04 11:24:16 +0700

    #

--- leetcode/src/main/java/number/CheckIfNumberIsASumOfPowerOf3.java
+++ leetcode/src/main/java/number/CheckIfNumberIsASumOfPowerOf3.java
@@ -2,6 +2,9 @@ package number;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /*
@@ -13,22 +16,47 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class CheckIfNumberIsASumOfPowerOf3 {
 
-    // Một số chia 3 sẽ dư 2, 1, 0
-    public boolean checkPowersOfThree(int n) {
+    // Một số chia 3 sẽ dư 0, 1, 2
+    public boolean checkPowersOfThree_OK(int n) {
         // 1 = 3^0
         if (n == 1) return true;
-
         // Chắc chắc là false
         if (n % 3 == 2) return false;
-
-        return checkPowersOfThree(n / 3);
+        // Đệ quy
+        return checkPowersOfThree_OK(n / 3);
     }
+
+    public boolean checkPowersOfThree(int n) {
+        while (n > 1) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+
+//    // Một số chia 3 sẽ dư 2, 1, 0
+//    public boolean checkPowersOfThree(int n) {
+//        List<Integer> pow3s = new ArrayList<>();
+//        for (int i = 0; (int) Math.pow(3, i) <= n; i++) {
+//            pow3s.add((int) Math.pow(3, i));
+//        }
+//        return go(pow3s, 0, n);
+//    }
+//
+//    public boolean go(List<Integer> pt, int idx, int n) {
+//        if (idx == pt.size()) {
+//            return n == 0;
+//        }
+//        return go(pt, idx + 1, n) || go(pt, idx + 1, n - pt.get(idx));
+//    }
 
     @Test
     public void test() {
         // assertEquals(true, checkPowersOfThree(3));
-        assertEquals(true, checkPowersOfThree(9));
-        assertEquals(true, checkPowersOfThree(12));
+        // assertEquals(true, checkPowersOfThree(9));
+        // assertEquals(true, checkPowersOfThree(12));
         assertEquals(true, checkPowersOfThree(91));
     }
 
