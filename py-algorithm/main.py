ea1cc6e92c7d5a318cf9627b5e7b3e521e38364c
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2025-05-31 21:05:58 +0700

    #

--- py-algorithm/main.py
+++ py-algorithm/main.py
@@ -1,6 +1,19 @@
-import re
+lst1 = [
+    {'firstName': 'Noah', 'lastName': 'M.', 'country': 'Switzerland', 'continent': 'Europe', 'age': 19,
+     'language': 'JavaScript'},
+    {'firstName': 'Maia', 'lastName': 'S.', 'country': 'Tahiti', 'continent': 'Oceania', 'age': 28,
+     'language': 'JavaScript'},
+    {'firstName': 'Shufen', 'lastName': 'L.', 'country': 'Taiwan', 'continent': 'Asia', 'age': 35, 'language': 'HTML'},
+    {'firstName': 'Sumayah', 'lastName': 'M.', 'country': 'Tajikistan', 'continent': 'Asia', 'age': 30,
+     'language': 'CSS'}
+]
 
-"""
-    Nếu x = ',' thì cộng trực tiếp
-    Nếu x != ',' thì cộng x + ' '
-"""
+
+def count_developers(lst):
+    # count = 0
+    # for x in lst:
+    #     if x['language'] == 'JavaScript' and x['continent'] == 'Europe':
+    #         count += 1
+    #
+    # return count
+    return sum(1 for x in lst if x['language'] == 'JavaScript' and x['continent'] == 'Europe')
