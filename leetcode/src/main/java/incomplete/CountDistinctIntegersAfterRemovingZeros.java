98b3aef32ddad9135d79f896d8de7c0a0525d285
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2025-12-03 14:59:09 +0700

    Create CountDistinctIntegersAfterRemovingZeros.java

---
+++ leetcode/src/main/java/incomplete/CountDistinctIntegersAfterRemovingZeros.java
@@ -0,0 +1,17 @@
+package incomplete;
+
+/*-
+    https://leetcode.com/problems/count-distinct-integers-after-removing-zeros/
+
+    Cho một số nguyên dương n. Với mỗi số nguyên x từ 1 -> n, ta viết ra số thu được
+    bằng cách xóa tất cả các chữ số 0 trong biểu diễn thập phân của x.
+
+    Hãy trả về số lượng các số khác nhau thu được sau khi thực hiện thao tác trên cho
+    tất cả các x từ 1 -> n.
+
+    Ví dụ:
+        x = 1010    → xóa tất cả chữ số 0 → còn lại 11
+        x = 10203   → xóa 0 → còn lại 123
+ */
+public class CountDistinctIntegersAfterRemovingZeros {
+}
